573597dbd6bfe028e9f44df2a10d2dc7d37045ff
Ultracatgirl <zeroaninea@gmail.com>
2025-03-25 02:26:46 +0400

    comments are deleted in the websocket.service.ts file

--- frontend/src/app/websocket.service.ts
+++ frontend/src/app/websocket.service.ts
@@ -1,28 +1,10 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
-// import * as io from 'socket.io-client';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService implements OnDestroy {
-  // private socket = io('http://localhost:3000');
-
-  // sendMessage(message: string){
-  //   this.socket.emit('new-message', message);
-  // }
-
-  // getMessages() {
-  //   let observable = new Observable<{ user: String, message: String }>(observer => {
-  //     this.socket.on('new-message', (data) => {
-  //       observer.next(data);
-  //     });
-  //     return () => { this.socket.disconnect(); };
-  //   });
-  //   return observable;
-  // }
-
   private socket: Socket;
 
   constructor() {
