df53c8f52caee557fc6fcaf88e0d9e1736e61813
Catgirl <zeroaninea@gmail.com>
2025-04-03 00:37:23 +0400

    MongoDB connection error: getaddrinfo EAI_AGAIN

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,4 +1,12 @@
 import mongoose from "mongoose";
 import { DB_URL } from "./env";
 
-mongoose.connect(DB_URL, {});
+const conn = mongoose.createConnection(DB_URL);
+
+conn.on("connected", (): void => {
+  console.log(" ✅ MongoDB successfully connected! 🎉 ");
+});
+
+conn.on("error", (err: Error) => {
+  console.error(" ❌ ", err);
+});
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,4 +7,4 @@ export const DB_PORT: number | string = parseInt(process.env.DB_PORT!);
 export const DB_NAME: string = process.env.DB_NAME!;
 export const DB_USER: string = process.env.DB_USER!;
 export const DB_PASSWORD: string = process.env.DB_PASSWORD!;
-export const DB_URL: string = `${DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export const DB_URL: string = `${DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
