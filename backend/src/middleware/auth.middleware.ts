6cdf71f72957129624cc27b281ee23f799e6f962
Catgirl <zeroaninea@gmail.com>
2025-04-03 21:01:56 +0400

    trying to handle the error cannot set headers

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -14,7 +14,13 @@ export const authMiddleware = (
   res: Response,
   next: NextFunction
 ): void => {
-  const token = req.header('Authorization')?.split(' ')[1];
+  let token: string | undefined;
+
+  if (!req.header('Authorization')) {
+    res.status(401).json({ message: 'Access denied. No headers provided.' });
+  } else {
+    token = req.header('Authorization')?.split(' ')[1];
+  }
 
   if (!token) {
     res.status(401).json({ message: 'Access denied. No token provided.' });
