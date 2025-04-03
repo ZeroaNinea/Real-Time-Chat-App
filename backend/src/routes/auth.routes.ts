b4dc6c24a66a8d59d567bbee91d31b5eb6816ecb
Catgirl <zeroaninea@gmail.com>
2025-04-03 04:31:52 +0400

    \n removed in auth.routes.ts

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,9 +4,7 @@ import { account, login, register } from '../controllers/auth.controller';
 const router = express.Router();
 
 router.post('/register', register);
-
 router.post('/login', login);
-
 router.get('/protected', account);
 
 export default router;
