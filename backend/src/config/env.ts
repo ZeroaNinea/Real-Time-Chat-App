24565e2d14ea3d3a1b8b44d59c8a8de32dae0475
Catgirl <zeroaninea@gmail.com>
2025-04-03 18:58:36 +0400

    env var PORT created

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Database
 export const DIALECT: string = process.env.DIALECT!;
 export const DB_HOST: string = process.env.DB_HOST!;
 export const DB_PORT: number | string = parseInt(process.env.DB_PORT!);
@@ -8,3 +9,6 @@ export const DB_NAME: string = process.env.DB_NAME!;
 export const DB_USER: string = process.env.DB_USER!;
 export const DB_PASSWORD: string = process.env.DB_PASSWORD!;
 export const DB_URL: string = `${DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
+// Application
+export const PORT: number = parseInt(process.env.PORT!) || 3000;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import { app } from './app';
 import { setupSocket } from './socket';
 import http from 'http';
 
+import { PORT } from './config/env';
+
 // Importing the database into the `server.ts` ensures that the database connection is established before the server starts and the application is read to work with the database.
 import './config/db';
 
@@ -9,5 +11,4 @@ import './config/db';
 const server = http.createServer(app);
 setupSocket(server);
 
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
