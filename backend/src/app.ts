90050d34a3fbcd02a0b278ac847d1fb6314b2c3a
Catgirl <zeroaninea@gmail.com>
2025-04-03 18:48:53 +0400

    the database reimported in the server.ts file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 
 import router from './routes';
-import './config/db';
 
 const app = express();
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,9 @@ import { app } from './app';
 import { setupSocket } from './socket';
 import http from 'http';
 
+// Importing the database into the `server.ts` ensures that the database connection is established before the server starts and the application is read to work with the database.
+import './config/db';
+
 // This file is the entry point of the backend server.
 const server = http.createServer(app);
 setupSocket(server);
