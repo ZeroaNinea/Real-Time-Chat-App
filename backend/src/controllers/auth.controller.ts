7c179a095da5f100359b9dd8726c68e9149695ee
Catgirl <zeroaninea@gmail.com>
2025-04-03 21:24:01 +0400

    searching for the reason of 500 status

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -51,7 +51,9 @@ export const register = async (req: Request, res: Response) => {
 
     res.status(201).json({ message: 'User registered successfully!' });
   } catch (error) {
-    res.status(500).json({ error: 'Server error duing registration.' });
+    res
+      .status(500)
+      .json({ error: 'Server error duing registration.', errorMessage: error });
   }
 };
 
