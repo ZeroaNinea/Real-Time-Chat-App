6a33adb0e16d0d90c88190e3798680b456ac9679
Catgirl <zeroaninea@gmail.com>
2025-04-05 22:00:24 +0400

    trying to connect MongoDB Memory Server with a callback hell

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,29 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { DB_URL } from './env';
+import { DB_URL, NODE_ENV } from './env';
 
-mongoose
-  .connect(DB_URL, {
-    serverSelectionTimeoutMS: 5000,
-  })
-  .then(() => console.log(' ✅ MongoDB successfully connected! 🎉 '))
-  .catch((err) => console.error(' ❌ MongoDB connection error:', err));
+if (NODE_ENV === 'test') {
+  MongoMemoryServer.create().then((mongoServer) => {
+    const TEST_DB_URL = mongoServer.getUri();
+
+    mongoose
+      .connect(TEST_DB_URL, {
+        serverSelectionTimeoutMS: 5000,
+      })
+      .then(() =>
+        console.log(' ✅ MongoDB Memory Server successfully connected! 🎉 ')
+      )
+      .catch((err) =>
+        console.error(' ❌ MongoDB Memory Server connection error:', err)
+      );
+  });
+} else {
+  mongoose
+    .connect(DB_URL, {
+      serverSelectionTimeoutMS: 5000,
+    })
+    .then(() => console.log(' ✅ MongoDB successfully connected! 🎉 '))
+    .catch((err) => console.error(' ❌ MongoDB connection error:', err));
+}
 
 export default mongoose;
