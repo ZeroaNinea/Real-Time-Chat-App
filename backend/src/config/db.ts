436f5898325dfe0ab5f5f68af903b42690fd2c2d
Catgirl <zeroaninea@gmail.com>
2025-04-05 22:12:55 +0400

    MongoDB Memory Server connection improved

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,29 +1,34 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { DB_URL, NODE_ENV } from './env';
 
-if (NODE_ENV === 'test') {
-  MongoMemoryServer.create().then((mongoServer) => {
-    const TEST_DB_URL = mongoServer.getUri();
+let mongoServer: MongoMemoryServer | null = null;
 
-    mongoose
-      .connect(TEST_DB_URL, {
-        serverSelectionTimeoutMS: 5000,
-      })
-      .then(() =>
-        console.log(' ✅ MongoDB Memory Server successfully connected! 🎉 ')
-      )
-      .catch((err) =>
-        console.error(' ❌ MongoDB Memory Server connection error:', err)
-      );
-  });
-} else {
-  mongoose
-    .connect(DB_URL, {
-      serverSelectionTimeoutMS: 5000,
-    })
-    .then(() => console.log(' ✅ MongoDB successfully connected! 🎉 '))
-    .catch((err) => console.error(' ❌ MongoDB connection error:', err));
-}
+export const connectToDatabase = async () => {
+  try {
+    if (NODE_ENV === 'test') {
+      mongoServer = await MongoMemoryServer.create();
+      const uri = mongoServer.getUri();
+      await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
+      console.log(' ✅ MongoDB Memory Server successfully connected! 🎉 ');
+    } else {
+      await mongoose.connect(DB_URL, { serverSelectionTimeoutMS: 5000 });
+      console.log(' ✅ MongoDB successfully connected! 🎉 ');
+    }
+  } catch (err) {
+    console.error(' ❌ MongoDB connection error:', err);
+  }
+};
+
+export const disconnectDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    if (mongoServer) {
+      await mongoServer.stop();
+    }
+  } catch (err) {
+    console.error(' ❌ MongoDB disconnection error:', err);
+  }
+};
 
 export default mongoose;
