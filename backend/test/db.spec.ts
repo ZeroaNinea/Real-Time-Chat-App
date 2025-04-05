0cfbad8b22998daa21a00917576955bdce4001fb
Catgirl <zeroaninea@gmail.com>
2025-04-05 22:56:52 +0400

    NODE_ENV should be test

--- backend/test/db.spec.ts
+++ backend/test/db.spec.ts
@@ -1,9 +1,13 @@
 import { expect } from 'chai';
 import { connectToDatabase, disconnectDatabase } from '../src/config/db';
 
-describe('test', () => {
+describe('Test Database Connection', () => {
   before(async () => {
     await connectToDatabase();
+  });
+
+  it('NEDE_ENV shoub be test', () => {
+    expect(process.env.NODE_ENV).to.equal('test');
   });
 
   after(async () => {
