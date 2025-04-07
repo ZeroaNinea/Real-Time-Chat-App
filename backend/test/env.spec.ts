e29c38b0461763a7ab2f6ee6889bdcb617376ebc
Catgirl <zeroaninea@gmail.com>
2025-04-07 04:30:01 +0400

    it's not possible to test these files I should rewrite them

--- backend/test/env.spec.ts
+++ backend/test/env.spec.ts
@@ -1,13 +1,4 @@
 import { expect } from 'chai';
-import {
-  DIALECT,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  PORT,
-} from '../src/config/env';
 
 describe('Environment Variables', () => {
   it('should load environment variables from .env file', async () => {
@@ -36,52 +27,24 @@ describe('Environment Variables', () => {
     expect(env.DB_PASSWORD).to.equal('testpass');
 
     expect(env.DB_URL).to.equal(
-      'mongodb://testuser:testpass@localhost:27017/testdb?authSource=admin'
+      `${env.DIALECT}://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?authSource=admin`
     );
 
     // Restore.
     process.env = originalEnv;
   });
 
-  it('should check environment variables datatypes', async () => {
+  it('should use default PORT if environment variable is not set', async () => {
+    const originalEnv = { ...process.env };
+    delete process.env.PORT;
+
+    delete require.cache[require.resolve('../src/config/env')];
     const env = await import('../src/config/env');
 
-    expect(env.DB_URL).to.be.a('string');
-    expect(env.NODE_ENV).to.be.a('string');
-    expect(env.PORT).to.be.a('number');
-    expect(env.DIALECT).to.be.a('string');
-    expect(env.DB_HOST).to.be.a('string');
-    expect(env.DB_PORT).to.be.a('number');
-    expect(env.DB_NAME).to.be.a('string');
-    expect(env.DB_USER).to.be.a('string');
-    expect(env.DB_PASSWORD).to.be.a('string');
+    console.log(process.env.PORT, '======================'); // Undefined.
+
+    expect(env.PORT).to.equal(3000); // 5000.
+
+    process.env = originalEnv;
   });
-
-  it('should check DB_PORT value', async () => {
-    expect(DB_PORT).to.be.a('number');
-
-    process.env.DB_PORT = '27017';
-
-    expect(DB_PORT).to.not.be.equal('27017', 'DB_PORT should be 27017');
-  });
-
-  it('should check PORT value', async () => {
-    expect(PORT).to.be.a('number');
-
-    process.env.PORT = '27017';
-
-    expect(PORT).to.not.be.equal('27017', 'DB_PORT should be 27017');
-  });
-
-  // it('should use default PORT if environment variable is not set', async () => {
-  //   const originalEnv = { ...process.env };
-  //   delete process.env.PORT;
-
-  //   delete require.cache[require.resolve('../src/config/env')];
-  //   const env = await import('../src/config/env');
-
-  //   expect(env.PORT).to.equal(3000);
-
-  //   process.env = originalEnv;
-  // });
 });
