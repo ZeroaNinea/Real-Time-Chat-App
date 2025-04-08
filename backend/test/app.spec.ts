434c158d211e49daa833fa265710e8e73b7a6f46
Catgirl <zeroaninea@gmail.com>
2025-04-08 20:10:56 +0400

    user registration tested

--- backend/test/app.spec.ts
+++ backend/test/app.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { expect } from 'chai';
 import { app } from '../src/app';
 import { server } from '../src/server';
+import { disconnectDatabase } from '../src/config/db';
 
 describe('Test App Router', () => {
   it('should register a new user', async () => {
@@ -19,12 +20,17 @@ describe('Test App Router', () => {
     expect(res.body.message).to.equal('User registered successfully!');
     console.log(res.body.message, res.status, '============================');
   });
-  // it('should return 401 for /auth/account', async () => {
-  //   const res = await request(app).get('/auth/account');
-  //   expect(res.status).to.equal(401);
-  //   expect(res.body.message).to.equal('Access denied. No headers provided.');
-  // });
-  after((done) => {
-    server.close(done);
+
+  it('should return 401 for /auth/account', async () => {
+    const res = await request(app).get('/auth/account');
+    expect(res.status).to.equal(401);
+    expect(res.body.message).to.equal('Access denied. No headers provided.');
+  });
+
+  after(async () => {
+    await disconnectDatabase();
+    server.close(async () => {
+      console.log('Server and database connections closed');
+    });
   });
 });
