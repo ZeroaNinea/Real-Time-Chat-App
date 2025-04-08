a621aa2c36c4899c0d10586deb9dcd726bd024c6
Catgirl <zeroaninea@gmail.com>
2025-04-08 19:30:40 +0400

    /auth/account 401 response tested

--- backend/test/app.spec.ts
+++ backend/test/app.spec.ts
@@ -3,8 +3,9 @@ import { expect } from 'chai';
 import { app } from '../src/app';
 
 describe('Test App Router', () => {
-  it('should return 200 for /auth/register', async () => {
-    const res = await request(app).get('/auth/register');
-    expect(res.status).to.equal(200);
+  it('should return 401 for /auth/account', async () => {
+    const res = await request(app).get('/auth/account');
+    expect(res.status).to.equal(401);
+    expect(res.body.message).to.equal('Access denied. No headers provided.');
   });
 });
