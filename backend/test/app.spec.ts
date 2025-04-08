64a3cf9fc4bbceb31fabe1026a3ff5132af17c98
Catgirl <zeroaninea@gmail.com>
2025-04-08 19:39:03 +0400

    don't see the coverage during the registration test

--- backend/test/app.spec.ts
+++ backend/test/app.spec.ts
@@ -1,11 +1,26 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import { app } from '../src/app';
+import { server } from '../src/server';
 
 describe('Test App Router', () => {
+  it('should register a new user', async () => {
+    const res = await request(app).post('/auth/register').send({
+      username: 'imgay',
+      email: 'imgay@gmail.com',
+      password: 'imgay',
+    });
+    expect(res.status).to.equal(201);
+    expect(res.body.message).to.equal('User registered successfully!');
+  });
+
   it('should return 401 for /auth/account', async () => {
     const res = await request(app).get('/auth/account');
     expect(res.status).to.equal(401);
     expect(res.body.message).to.equal('Access denied. No headers provided.');
+  });
+
+  after((done) => {
+    server.close(done);
   });
 });
