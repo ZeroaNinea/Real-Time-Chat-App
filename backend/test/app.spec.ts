7b432bde474c750de24398cd3afc8b97c1a1d922
Catgirl <zeroaninea@gmail.com>
2025-04-08 19:45:34 +0400

    response data logged

--- backend/test/app.spec.ts
+++ backend/test/app.spec.ts
@@ -5,13 +5,19 @@ import { server } from '../src/server';
 
 describe('Test App Router', () => {
   it('should register a new user', async () => {
-    const res = await request(app).post('/auth/register').send({
-      username: 'imgay',
-      email: 'imgay@gmail.com',
-      password: 'imgay',
-    });
+    const res = await request(app)
+      .post('/auth/register')
+      .send({
+        username: 'imgay',
+        email: 'imgay@gmail.com',
+        password: 'imgay',
+      })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
     expect(res.status).to.equal(201);
-    // expect(res.body.message).to.equal('User registered successfully!');
+    expect(res.body.message).to.equal('User registered successfully!');
+
+    console.log(res.body.message, res.status, '============================');
   });
 
   it('should return 401 for /auth/account', async () => {
