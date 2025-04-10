6e66986adcde41900ecb1c7ac0042ea9c07ff8b0
Catgirl <zeroaninea@gmail.com>
2025-04-10 18:45:37 +0400

    console.error added in auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -85,6 +85,7 @@ export const deleteAccount = async (req: Request, res: Response) => {
 
     res.status(200).json({ message: 'Account deleted successfully!' });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Server error during account deletion.' });
   }
 };
