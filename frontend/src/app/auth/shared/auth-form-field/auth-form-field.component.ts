c4e6f454564488907ae3fde50a7500ec63e2784a
Catgirl <zeroaninea@gmail.com>
2025-04-12 01:03:11 +0400

    standalone true added

--- frontend/src/app/auth/shared/auth-form-field/auth-form-field.component.ts
+++ frontend/src/app/auth/shared/auth-form-field/auth-form-field.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-form-field',
   imports: [],
+  standalone: true,
   templateUrl: './auth-form-field.component.html',
-  styleUrl: './auth-form-field.component.scss'
+  styleUrl: './auth-form-field.component.scss',
 })
-export class AuthFormFieldComponent {
-
-}
+export class AuthFormFieldComponent {}
