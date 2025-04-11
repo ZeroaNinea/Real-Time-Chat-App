2b08cfd5a4c6cbdecf59ebfb18db0f8274c73eb4
Catgirl <zeroaninea@gmail.com>
2025-04-11 20:18:38 +0400

    auth-form-field component created

---
+++ frontend/src/app/auth/shared/auth-form-field/auth-form-field.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form-field',
+  imports: [],
+  templateUrl: './auth-form-field.component.html',
+  styleUrl: './auth-form-field.component.scss'
+})
+export class AuthFormFieldComponent {
+
+}
