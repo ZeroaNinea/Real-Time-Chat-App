bf8ea5862ab6cd688add67eacd2030237ad17476
Catgirl <zeroaninea@gmail.com>
2025-04-14 13:38:48 +0400

    the interceptor updated

--- frontend/src/app/auth/shared/auth.interceptor.ts
+++ frontend/src/app/auth/shared/auth.interceptor.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('accessToken');
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  }
+
   return next(req);
 };
