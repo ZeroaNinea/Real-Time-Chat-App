65105e43ea4277a8563629204a1b0dd597fa22f9
Catgirl <zeroaninea@gmail.com>
2025-04-16 12:58:19 +0400

    UnauthorizedComponent created

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -2,11 +2,17 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('accessToken');
   const router = inject(Router);
 
+  if (typeof window === 'undefined') {
+    // Can't check tokens server-side.
+    return false;
+  }
+
+  const token = localStorage.getItem('accessToken');
+
   if (!token) {
-    router.navigate(['/login']);
+    router.navigate(['/401']); // Use a 401 page for better UX.
     return false;
   }
 
