85c86321b6656b5e1b6ee717e0b25a34761612f0
Catgirl <zeroaninea@gmail.com>
2025-04-16 12:32:25 +0400

    auth guard created

---
+++ frontend/src/app/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('accessToken');
+  const router = inject(Router);
+
+  if (!token) {
+    router.navigate(['/login']);
+    return false;
+  }
+
+  return true;
+};
