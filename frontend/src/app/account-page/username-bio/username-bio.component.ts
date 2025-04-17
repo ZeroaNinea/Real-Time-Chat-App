1388f27c934b53197711bc6c5fee8113f6cc5200
Catgirl <zeroaninea@gmail.com>
2025-04-17 15:41:29 +0400

    user input created

--- frontend/src/app/account-page/username-bio/username-bio.component.ts
+++ frontend/src/app/account-page/username-bio/username-bio.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AuthFormFieldComponent } from '../../auth/shared/auth-form-field/auth-form-field.component';
 
 @Component({
   selector: 'app-username-bio',
-  imports: [],
+  imports: [
+    AuthFormFieldComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   standalone: true,
   templateUrl: './username-bio.component.html',
   styleUrl: './username-bio.component.scss',
 })
-export class UsernameBioComponent {}
+export class UsernameBioComponent {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    username: [''],
+    bio: [''],
+  });
+
+  get usernameBioControl() {
+    return this.form.get(['username', 'bio']) as FormControl;
+  }
+}
