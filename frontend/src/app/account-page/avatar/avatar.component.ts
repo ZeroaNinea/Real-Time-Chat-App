78d50887181424c5df0d9b6094b9095be2e14e24
Catgirl <zeroaninea@gmail.com>
2025-04-19 00:30:16 +0400

    AvatarComponent created

---
+++ frontend/src/app/account-page/avatar/avatar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [],
+  standalone: true,
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss',
+})
+export class AvatarComponent {}
