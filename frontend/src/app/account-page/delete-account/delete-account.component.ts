87a9b1e782a15740975e74b99c4eb0fe6f05f5a5
Catgirl <zeroaninea@gmail.com>
2025-04-20 00:33:26 +0400

    DeleteAccountComponent created

---
+++ frontend/src/app/account-page/delete-account/delete-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-account',
+  imports: [],
+  standalone: true,
+  templateUrl: './delete-account.component.html',
+  styleUrl: './delete-account.component.scss',
+})
+export class DeleteAccountComponent {}
