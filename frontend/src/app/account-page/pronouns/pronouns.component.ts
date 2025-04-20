0cb20b1ce43f4e46cf5590fa2ce9b3c4dbbec96c
Catgirl <zeroaninea@gmail.com>
2025-04-20 20:51:10 +0400

    standalone: true added in pronouns.component.ts

--- frontend/src/app/account-page/pronouns/pronouns.component.ts
+++ frontend/src/app/account-page/pronouns/pronouns.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pronouns',
   imports: [],
+  standalone: true,
   templateUrl: './pronouns.component.html',
-  styleUrl: './pronouns.component.scss'
+  styleUrl: './pronouns.component.scss',
 })
-export class PronounsComponent {
-
-}
+export class PronounsComponent {}
