d6e83c8077de7bd188524b7da542bbf6aca22751
Catgirl <zeroaninea@gmail.com>
2025-04-20 23:21:55 +0400

    standalone: true, in the LogoutComponent

--- frontend/src/app/account-page/logout/logout.component.ts
+++ frontend/src/app/account-page/logout/logout.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-logout',
   imports: [],
+  standalone: true,
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.scss'
+  styleUrl: './logout.component.scss',
 })
-export class LogoutComponent {
-
-}
+export class LogoutComponent {}
