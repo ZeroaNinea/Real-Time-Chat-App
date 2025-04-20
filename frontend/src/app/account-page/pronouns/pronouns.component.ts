e5ec7e641e55428d5716fab6c17c73bb416964c4
Catgirl <zeroaninea@gmail.com>
2025-04-20 20:50:02 +0400

    PronounsComponent created

---
+++ frontend/src/app/account-page/pronouns/pronouns.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pronouns',
+  imports: [],
+  templateUrl: './pronouns.component.html',
+  styleUrl: './pronouns.component.scss'
+})
+export class PronounsComponent {
+
+}
