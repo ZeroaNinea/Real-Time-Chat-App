80d43f35861a8a730ea2c1c6b71be427b5d740c5
Catgirl <zeroaninea@gmail.com>
2025-04-21 22:37:01 +0400

    ChatSchema created

---
+++ backend/src/models/chat.model.ts
@@ -0,0 +1,29 @@
+import mongoose from '../config/db';
+import { Document } from 'mongoose';
+
+export interface IChat {
+  name: string;
+  isPrivate: boolean;
+  members: { type: mongoose.Schema.Types.ObjectId; ref: 'User' }[];
+}
+
+export interface ChatDocument extends IChat, Document {}
+
+const ChatSchema = new mongoose.Schema<ChatDocument>(
+  {
+    name: {
+      type: String,
+    },
+    isPrivate: {
+      type: Boolean,
+      required: true,
+      default: true,
+      members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model<ChatDocument>('Chat', ChatSchema);
