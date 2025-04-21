287da6c4a41bc1a943d2ad2968c0f9116cb620c0
Catgirl <zeroaninea@gmail.com>
2025-04-21 16:20:23 +0400

    UserCardComponent styled

--- frontend/src/app/account-page/user-card/user-card.component.ts
+++ frontend/src/app/account-page/user-card/user-card.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
 import { User } from '../../auth/shared/user.model';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-user-card',
-  imports: [DatePipe],
+  imports: [DatePipe, MatCardModule],
   standalone: true,
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
 export class UserCardComponent {
   @Input() user!: User;
+  environment = environment;
 }
