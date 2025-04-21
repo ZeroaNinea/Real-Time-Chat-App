be096fcb2b364308489b9fd631ab278877bd87ad
Catgirl <zeroaninea@gmail.com>
2025-04-21 22:56:12 +0400

    chats imported in routes/index.ts

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -2,8 +2,11 @@ import express from 'express';
 
 import { authMiddleware } from '../auth/auth.middleware';
 import { asyncRoute } from '../controllers/auth.controller';
-import { mine } from '../controllers/chat.controller';
+import { mine, privateMessages } from '../controllers/chat.controller';
 
 const router = express.Router();
 
 router.get('/mine', authMiddleware, asyncRoute(mine));
+router.get('/post', authMiddleware, asyncRoute(privateMessages));
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import authRoutes from './auth.routes';
+import chatRoutes from './chat.routes';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/chat', chatRoutes);
 
 export default router;
