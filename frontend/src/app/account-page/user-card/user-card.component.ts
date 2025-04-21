504b41d6aa9f93f0114522c0694fcd2a3bc841df
Catgirl <zeroaninea@gmail.com>
2025-04-21 14:09:00 +0400

    UserCardComponent created

---
+++ frontend/src/app/account-page/user-card/user-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss',
+})
+export class UserCardComponent {}
