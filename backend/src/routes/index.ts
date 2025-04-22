c8199c5b7535c06b3c1223cfc8be8c56b7de693d
Catgirl <zeroaninea@gmail.com>
2025-04-22 16:08:12 +0400

    messageRoutes imported in routes/index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import authRoutes from './auth.routes';
 import chatRoutes from './chat.routes';
+import messageRoutes from './message.routes';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/chat', chatRoutes);
+router.use('/message', messageRoutes);
 
 export default router;
