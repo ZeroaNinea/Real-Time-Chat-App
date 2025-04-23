a55a0edb5e7766c3be952066f538f11cb7e28a9b
Catgirl <zeroaninea@gmail.com>
2025-04-23 12:55:11 +0400

    the logic in the chat-room.component.ts created

--- frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
+++ frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  inject,
+  OnDestroy,
+  signal,
+} from '@angular/core';
+import { WebsocketService } from '../../shared/services/websocket/websocket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat-room',
@@ -7,4 +15,24 @@ import { Component } from '@angular/core';
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.scss',
 })
-export class ChatRoomComponent {}
+export class ChatRoomComponent implements OnDestroy {
+  message = signal('');
+  messages = signal<string[]>([]);
+  private wsService = inject(WebsocketService);
+  private sub?: Subscription;
+
+  constructor() {
+    afterNextRender(() => this.connect());
+  }
+
+  connect() {
+    this.wsService.connect();
+    this.sub = this.wsService.getMessages().subscribe((msgs) => {
+      this.messages.set(msgs);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+}
