f75ba8b0ceb823da6b8f06e9d21c3836c75722c6
Catgirl <zeroaninea@gmail.com>
2025-04-23 13:09:40 +0400

    logic is written in the MessageInputComponent

--- frontend/src/app/chat/chat-room-page/message-input/message-input.component.ts
+++ frontend/src/app/chat/chat-room-page/message-input/message-input.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-message-input',
-  imports: [],
+  imports: [FormsModule],
   standalone: true,
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.scss',
 })
-export class MessageInputComponent {}
+export class MessageInputComponent {
+  message!: string;
+  messageChange = new EventEmitter<string>();
+  send = new EventEmitter<void>();
+}
