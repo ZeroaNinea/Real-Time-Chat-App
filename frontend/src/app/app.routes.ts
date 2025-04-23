7318ce8ffe05dbfb9c042c363e231112d6e3af5a
Catgirl <zeroaninea@gmail.com>
2025-04-23 12:25:19 +0400

    app.routes.ts edited

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
-import { ChatComponent } from './chat/chat.component';
+import { ChatComponent } from './chat/chat-room-page/chat/chat.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account-page/account/account.component';
