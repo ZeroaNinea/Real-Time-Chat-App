d542680b76587d978807083785e09e5bcdc4db77
Catgirl <zeroaninea@gmail.com>
2025-04-23 12:56:13 +0400

    message-list.component.ts created

---
+++ frontend/src/app/chat/chat-room-page/message-list/message-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './message-list.component.html',
+  styleUrl: './message-list.component.scss',
+})
+export class MessageListComponent {}
