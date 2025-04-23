43456e053aba3a95fa8b39d2d40ede24e7175af0
Catgirl <zeroaninea@gmail.com>
2025-04-24 00:08:12 +0400

    backendUrl added in chat.service.ts

--- frontend/src/app/chat/shared/services/chat-service/chat.service.ts
+++ frontend/src/app/chat/shared/services/chat-service/chat.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Chat } from '../../models/chat.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class ChatService {
   constructor() {}
 
   createChatRoom(data: { name: string; channels: string }): Observable<Chat> {
-    return this.http.post<Chat>('/chat/create-chat', {
+    return this.http.post<Chat>(`${environment.backendUrl}/chat/create-chat`, {
       name: data.name,
       channels: data.channels,
       isPrivate: false,
