2be1a51b93d0af67aa6f826efd20df15c141b3a4
Catgirl <zeroaninea@gmail.com>
2025-04-25 12:56:23 +0400

    channel.model.ts created

---
+++ frontend/src/app/chat/shared/models/channel.model.ts
@@ -0,0 +1,12 @@
+interface Channel {
+  _id: string;
+  chatId: string;
+  name: string;
+  topic?: string;
+  permissions: {
+    adminsOnly?: boolean;
+    readOnly?: boolean;
+    allowedUsers?: string[];
+    allowedRoles?: string[];
+  };
+}
