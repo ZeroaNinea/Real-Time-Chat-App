eef591e8263917ba1babe667bf9c170e1bcbb3cc
Catgirl <zeroaninea@gmail.com>
2025-04-27 20:30:21 +0400

    .ts logic written

--- frontend/src/app/chat/channel-list/channel-list.component.ts
+++ frontend/src/app/chat/channel-list/channel-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Channel } from '../shared/models/channel.model';
 
 @Component({
   selector: 'app-channel-list',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './channel-list.component.html',
   styleUrl: './channel-list.component.scss',
 })
-export class ChannelListComponent {}
+export class ChannelListComponent {
+  @Input() channels: Channel[] = [];
+
+  @Output() removeChannel = new EventEmitter<string>();
+  @Output() renameChannel = new EventEmitter<{ id: string; newName: string }>();
+
+  onRemove(id: string) {
+    this.removeChannel.emit(id);
+  }
+
+  onRename(id: string, newName: string) {
+    this.renameChannel.emit({ id, newName });
+  }
+}
