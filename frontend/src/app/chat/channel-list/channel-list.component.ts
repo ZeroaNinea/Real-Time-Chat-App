4a86cd88fdd1e51e9ec309244c3e0fae25924f32
Catgirl <zeroaninea@gmail.com>
2025-04-27 20:15:07 +0400

    ChannelListComponent created

---
+++ frontend/src/app/chat/channel-list/channel-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './channel-list.component.html',
+  styleUrl: './channel-list.component.scss',
+})
+export class ChannelListComponent {}
