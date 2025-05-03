11438d256a930793ea593ace1a5ef56af60495e0
Catgirl <zeroaninea@gmail.com>
2025-05-03 18:46:35 +0400

    the old code deleted in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,3 @@
-// import { app } from './app';
-// import { setupSocket } from './socket';
-// import http from 'http';
-
-// import config from './config/env';
-// import { rotateKeys } from '../cryptography/rsa-keys-rotation';
-// import { connectToDatabase } from './config/db';
-
-// const { PORT } = config; // Get environment variables.
-// rotateKeys(); // Rotate RSA keys.
-
-// // Importing the database into the `server.ts` ensures that the database connection is established before the server starts and the application is read to work with the database.
-// connectToDatabase();
-
-// // This file is the entry point of the backend server.
-// const server = http.createServer(app);
-// setupSocket(server);
-
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
 import { app } from './app';
 import { setupSocket } from './socket';
 import http from 'http';
