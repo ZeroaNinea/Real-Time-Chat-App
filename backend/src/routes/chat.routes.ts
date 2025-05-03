1b3e70ccc7c3db24bfc1c6febbb86d1f823892bc
Catgirl <zeroaninea@gmail.com>
2025-05-03 09:05:08 +0400

    there is no an updateChat controller yet

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -19,6 +19,7 @@ router.get('/mine', authMiddleware, asyncRoute(mine));
 router.get('/post', authMiddleware, asyncRoute(privateMessages));
 
 router.post('/create-chat', authMiddleware, asyncRoute(createChat));
+// router.post('/update-chat/:chatId', authMiddleware, asyncRoute(updateChat));
 router.delete('/:chatId', authMiddleware, asyncRoute(deleteChat));
 router.get('/:chatId', authMiddleware, asyncRoute(getChat));
 
