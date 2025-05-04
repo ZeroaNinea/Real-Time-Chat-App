91948ef40a6a50848a7f844e59f83612954bc9f1
Catgirl <zeroaninea@gmail.com>
2025-05-04 17:50:41 +0400

    editChannel added in socket.ts

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,6 +1,7 @@
 import { Server } from 'socket.io';
 import { Server as HttpServer } from 'http';
 import { Express } from 'express';
+import { updateChannel } from './controllers/chat.controller';
 
 // This function sets up the Socket.io server and handles events.
 export function setupSocket(server: HttpServer, app: Express) {
@@ -29,6 +30,16 @@ export function setupSocket(server: HttpServer, app: Express) {
     socket.on('joinChatRoom', (chatId: string) => {
       socket.join(chatId);
       console.log(`Socket ${socket.id} joined room ${chatId}`);
+    });
+
+    socket.on('editChannel', ({ chatId, channel }) => {
+      updateChannel(chatId, channel)
+        .then(() => {
+          io.to(chatId).emit('channelEdited', { channel });
+        })
+        .catch((err: unknown) => {
+          console.error('Channel update failed:', err);
+        });
     });
   });
 
