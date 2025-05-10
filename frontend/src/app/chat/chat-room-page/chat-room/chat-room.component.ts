d93be39bb3e259b538da92816ca4725724ebcab8
Catgirl <zeroaninea@gmail.com>
2025-05-10 22:30:20 +0400

    trying to listen Socket.io events

--- frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
+++ frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
@@ -70,6 +70,7 @@ export class ChatRoomComponent implements OnDestroy {
 
         if (id) {
           this.fetchChatRoom(id);
+          this.setupRealtimeChannelUpdates(id);
         } else {
           this.isOwner.set(true);
           this.isAdmin.set(true);
@@ -131,6 +132,9 @@ export class ChatRoomComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
+    this.wsService.off('channel-added');
+    this.wsService.off('channel-renamed');
+    this.wsService.off('channel-deleted');
   }
 
   addChannel(name: string) {
@@ -235,6 +239,29 @@ export class ChatRoomComponent implements OnDestroy {
         _id: id,
         name: newName,
       },
+    });
+  }
+
+  setupRealtimeChannelUpdates(chatId: string) {
+    this.wsService.emit('join-room', chatId);
+
+    this.wsService.on('channel-added', (channel: Channel) => {
+      this.channels.set([...this.channels(), channel]);
+    });
+
+    this.wsService.on(
+      'channel-renamed',
+      ({ id, newName }: { id: string; newName: string }) => {
+        this.channels.set(
+          this.channels().map((c) =>
+            c._id === id ? { ...c, name: newName } : c
+          )
+        );
+      }
+    );
+
+    this.wsService.on('channel-deleted', (id: string) => {
+      this.channels.set(this.channels().filter((c) => c._id !== id));
     });
   }
 }
