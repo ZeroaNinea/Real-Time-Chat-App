54a4b529509a58a85c0c0cf250c120315060222a
Catgirl <zeroaninea@gmail.com>
2025-05-13 17:07:09 +0400

    socket-extensions.d.ts added

--- backend/types/socket-extensions.d.ts
+++ backend/types/socket-extensions.d.ts
@@ -1,5 +1,8 @@
-// declare module 'socket.io' {
-//   interface Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> {
-//     user: UserDocument;
-//   }
-// }
+import { UserDocument } from '../models/user.model';
+import 'socket.io';
+
+declare module 'socket.io' {
+  interface Socket {
+    user: UserDocument;
+  }
+}
