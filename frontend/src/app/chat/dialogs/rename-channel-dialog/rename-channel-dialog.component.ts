347ca49c68c5821bf81b35483fa244b11555ae43
Catgirl <zeroaninea@gmail.com>
2025-05-14 00:22:14 +0400

    RenameChannelDialogComponent's logic created

--- frontend/src/app/chat/dialogs/rename-channel-dialog/rename-channel-dialog.component.ts
+++ frontend/src/app/chat/dialogs/rename-channel-dialog/rename-channel-dialog.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-rename-channel-dialog',
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule],
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule],
   standalone: true,
   templateUrl: './rename-channel-dialog.component.html',
   styleUrl: './rename-channel-dialog.component.scss',
 })
-export class RenameChannelDialogComponent {}
+export class RenameChannelDialogComponent {
+  newName: string;
+
+  constructor(
+    private dialogRef: MatDialogRef<RenameChannelDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { currentName: string }
+  ) {
+    this.newName = data.currentName;
+  }
+
+  onCancel() {
+    this.dialogRef.close(null);
+  }
+
+  onSave() {
+    this.dialogRef.close(this.newName.trim());
+  }
+}
