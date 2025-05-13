a38e608dd996c63e5b8e73fe2e3d106c3b0f0a4a
Catgirl <zeroaninea@gmail.com>
2025-05-13 23:51:11 +0400

    standalone: true, in rename-channel-dialog.component.ts

--- frontend/src/app/chat/dialogs/rename-channel-dialog/rename-channel-dialog.component.ts
+++ frontend/src/app/chat/dialogs/rename-channel-dialog/rename-channel-dialog.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rename-channel-dialog',
   imports: [],
+  standalone: true,
   templateUrl: './rename-channel-dialog.component.html',
-  styleUrl: './rename-channel-dialog.component.scss'
+  styleUrl: './rename-channel-dialog.component.scss',
 })
-export class RenameChannelDialogComponent {
-
-}
+export class RenameChannelDialogComponent {}
