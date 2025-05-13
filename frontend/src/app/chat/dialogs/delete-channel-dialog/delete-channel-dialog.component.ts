df8c2da19de02ef8f58f5574c81452ffea9b15c7
Catgirl <zeroaninea@gmail.com>
2025-05-14 02:09:27 +0400

    the DeleteChannelDialogComponent's logic is written

--- frontend/src/app/chat/dialogs/delete-channel-dialog/delete-channel-dialog.component.ts
+++ frontend/src/app/chat/dialogs/delete-channel-dialog/delete-channel-dialog.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { RenameChannelDialogComponent } from '../rename-channel-dialog/rename-channel-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-delete-channel-dialog',
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule],
   standalone: true,
   templateUrl: './delete-channel-dialog.component.html',
   styleUrl: './delete-channel-dialog.component.scss',
 })
-export class DeleteChannelDialogComponent {}
+export class DeleteChannelDialogComponent {
+  constructor(private dialogRef: MatDialogRef<RenameChannelDialogComponent>) {}
+
+  onCancel() {
+    this.dialogRef.close(null);
+  }
+
+  onDelete() {
+    this.dialogRef.close(true);
+  }
+}
