38a47ae96654c6708a37643ea4123539315270c6
Catgirl <zeroaninea@gmail.com>
2025-05-14 22:17:02 +0400

    ChannelTopicComponent's logic is written

--- frontend/src/app/chat/chat-room-page/channel-topic/channel-topic.component.ts
+++ frontend/src/app/chat/chat-room-page/channel-topic/channel-topic.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -20,4 +20,25 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './channel-topic.component.html',
   styleUrl: './channel-topic.component.scss',
 })
-export class ChannelTopicComponent {}
+export class ChannelTopicComponent {
+  @Input() topic: string | undefined;
+  @Input() canEdit = false;
+  @Output() topicChange = new EventEmitter<string>();
+
+  editing = false;
+  draft = '';
+
+  startEdit() {
+    this.draft = this.topic ?? '';
+    this.editing = true;
+  }
+
+  save() {
+    this.editing = false;
+    this.topicChange.emit(this.draft);
+  }
+
+  cancel() {
+    this.editing = false;
+  }
+}
