50bcaf1d0ff1f4ef647e138cf8fd67c800c0ff6f
Catgirl <zeroaninea@gmail.com>
2025-05-15 14:41:49 +0400

    basic TS is written

--- frontend/src/app/chat/dialogs/permissions-dialog/permissions-dialog.component.ts
+++ frontend/src/app/chat/dialogs/permissions-dialog/permissions-dialog.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
@@ -18,4 +22,10 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './permissions-dialog.component.html',
   styleUrl: './permissions-dialog.component.scss',
 })
-export class PermissionsDialogComponent {}
+export class PermissionsDialogComponent {
+  constructor(
+    private dialogRef: MatDialogRef<PermissionsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {}
+  ) {}
+}
