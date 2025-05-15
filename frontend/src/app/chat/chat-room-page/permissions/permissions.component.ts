ef83acfd3a5c26ff9ba885e3f5ef32947e676072
Catgirl <zeroaninea@gmail.com>
2025-05-15 12:55:09 +0400

    PermissionsComponent created

---
+++ frontend/src/app/chat/chat-room-page/permissions/permissions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permissions',
+  imports: [],
+  standalone: true,
+  templateUrl: './permissions.component.html',
+  styleUrl: './permissions.component.scss',
+})
+export class PermissionsComponent {}
