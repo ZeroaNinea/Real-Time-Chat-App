5a65ebd4e1f8e47b2d9246e74a590636b5244627
Catgirl <zeroaninea@gmail.com>
2025-05-15 14:36:14 +0400

    required moduls imported in PermissionsDialogComponent

--- frontend/src/app/chat/dialogs/permissions-dialog/permissions-dialog.component.ts
+++ frontend/src/app/chat/dialogs/permissions-dialog/permissions-dialog.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-permissions-dialog',
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   standalone: true,
   templateUrl: './permissions-dialog.component.html',
   styleUrl: './permissions-dialog.component.scss',
