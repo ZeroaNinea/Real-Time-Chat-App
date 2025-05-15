86c6dc5c49d70266fb0298f5e5c084831c14af1a
Catgirl <zeroaninea@gmail.com>
2025-05-15 12:56:35 +0400

    PermissionsDialogComponent created

---
+++ frontend/src/app/chat/dialogs/permissions-dialog/permissions-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permissions-dialog',
+  imports: [],
+  standalone: true,
+  templateUrl: './permissions-dialog.component.html',
+  styleUrl: './permissions-dialog.component.scss',
+})
+export class PermissionsDialogComponent {}
