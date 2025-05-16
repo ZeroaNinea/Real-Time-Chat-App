ff546dc78fec3766dd13d3fc8efa860991e5170a
Catgirl <zeroaninea@gmail.com>
2025-05-16 20:42:47 +0400

    messageSubject = new BehaviorSubject<Message[]>([]);

--- frontend/src/app/chat/shared/services/websocket/websocket.service.ts
+++ frontend/src/app/chat/shared/services/websocket/websocket.service.ts
@@ -4,6 +4,7 @@ import { io, Socket } from 'socket.io-client';
 
 import { environment } from '../../../../../environments/environment';
 import { Channel } from '../../models/channel.model';
+import { Message } from '../../models/message.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ import { Channel } from '../../models/channel.model';
 export class WebsocketService implements OnDestroy {
   private socket!: Socket;
   private isConnected = false;
-  private messageSubject = new BehaviorSubject<string[]>([]);
+  private messageSubject = new BehaviorSubject<Message[]>([]);
   private pendingJoins: string[] = [];
 
   private flushPendingJoins() {
