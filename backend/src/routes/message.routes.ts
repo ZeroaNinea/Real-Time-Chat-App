9257a325312736a74d6834a8e788f3549fdd2845
Catgirl <zeroaninea@gmail.com>
2025-05-16 21:52:50 +0400

    the message.routes.ts file fixed

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { authMiddleware } from '../auth/auth.middleware';
 import { asyncRoute } from '../controllers/auth.controller';
-import { sendMessage, getMessages } from '../controllers/message.controller';
+import { getMessages } from '../controllers/message.controller';
 
 const router = express.Router();
 
-router.post('/', authMiddleware, asyncRoute(sendMessage));
+// router.post('/', authMiddleware, asyncRoute(sendMessage));
 router.get('/:chatId', authMiddleware, asyncRoute(getMessages));
 
 export default router;
