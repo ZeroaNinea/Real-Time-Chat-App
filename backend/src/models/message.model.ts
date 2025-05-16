e323c3eb3b3bb3b249fa9d72cbd2000e3e8e3a97
Catgirl <zeroaninea@gmail.com>
2025-05-16 15:00:31 +0400

    message.model.ts updated

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -2,7 +2,8 @@ import mongoose from '../config/db';
 import { Document } from 'mongoose';
 
 export interface IMessage {
-  chat: mongoose.Types.ObjectId;
+  chatRoomId: mongoose.Types.ObjectId;
+  channelId: mongoose.Types.ObjectId;
   sender: mongoose.Types.ObjectId;
   text: string;
   timestamp?: Date;
@@ -12,9 +13,14 @@ export interface MessageDocument extends IMessage, Document {}
 
 const MessageSchema = new mongoose.Schema<MessageDocument>(
   {
-    chat: {
+    chatRoomId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Chat',
+      required: true,
+    },
+    channelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Channel',
       required: true,
     },
     sender: {
