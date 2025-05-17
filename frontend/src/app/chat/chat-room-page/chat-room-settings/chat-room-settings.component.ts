6f7c2d7b4ca3c2cbc84429b33cfdcb265783dffd
Catgirl <zeroaninea@gmail.com>
2025-05-17 23:33:53 +0400

    MAT_FORM_FIELD_DEFAULT_OPTIONS

--- frontend/src/app/chat/chat-room-page/chat-room-settings/chat-room-settings.component.ts
+++ frontend/src/app/chat/chat-room-page/chat-room-settings/chat-room-settings.component.ts
@@ -2,7 +2,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Channel } from '../../shared/models/channel.model';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
@@ -11,6 +14,14 @@ import { MatInputModule } from '@angular/material/input';
   standalone: true,
   templateUrl: './chat-room-settings.component.html',
   styleUrl: './chat-room-settings.component.scss',
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic',
+      },
+    },
+  ],
 })
 export class ChatRoomSettingsComponent {
   @Input() chatName = '';
