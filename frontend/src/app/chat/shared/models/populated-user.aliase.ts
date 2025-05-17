971ffd34ac99fe20d677069f20d875e549c7b7f7
Catgirl <zeroaninea@gmail.com>
2025-05-17 18:13:17 +0400

    old PopulatedUser is removed

--- frontend/src/app/chat/shared/models/populated-user.aliase.ts
+++ frontend/src/app/chat/shared/models/populated-user.aliase.ts
@@ -1,10 +1,3 @@
-// export type PopulatedUser = {
-//   _id: string;
-//   username: string;
-//   avatar: string;
-//   pronouns: string;
-// };
-
 export interface PopulatedUser {
   user: {
     _id: string;
