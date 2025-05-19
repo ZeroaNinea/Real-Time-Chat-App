8535667d7acd86ff5b4588e83816fb454c8c2b52
Catgirl <zeroaninea@gmail.com>
2025-05-19 20:01:36 +0400

    message list scrolling implemented

--- frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
+++ frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
@@ -103,22 +103,52 @@ export class ChatRoomComponent
   }
 
   constructor() {
+    // effect(() => {
+    //   const msgs = this.messages();
+    //   const container = this.scrollContainer?.nativeElement;
+
+    //   if (!container) return;
+
+    //   const newMessageCount = msgs.length;
+
+    //   if (newMessageCount > this.lastMessageCount && this.isAtBottom()) {
+    //     // New message arrived and user was at bottom.
+    //     queueMicrotask(() => {
+    //       container.scrollTop = container.scrollHeight;
+    //     });
+    //   }
+
+    //   this.lastMessageCount = newMessageCount;
+    // });
     effect(() => {
+      // Scroll when messages change and user is at bottom.
       const msgs = this.messages();
       const container = this.scrollContainer?.nativeElement;
-
       if (!container) return;
 
       const newMessageCount = msgs.length;
 
       if (newMessageCount > this.lastMessageCount && this.isAtBottom()) {
-        // New message arrived and user was at bottom.
         queueMicrotask(() => {
           container.scrollTop = container.scrollHeight;
         });
       }
 
       this.lastMessageCount = newMessageCount;
+    });
+
+    effect(() => {
+      const currentChannel = this.channelId();
+      this.isAtBottom.set(true);
+
+      queueMicrotask(() => {
+        const container = this.scrollContainer?.nativeElement;
+        if (container) {
+          container.scrollTop = container.scrollHeight;
+        }
+      });
+
+      this.lastMessageCount = this.messages().length;
     });
 
     afterNextRender(() => {
