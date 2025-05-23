f65035821fe5dda72be432978df36f1777aa265c
Catgirl <zeroaninea@gmail.com>
2025-05-23 21:32:59 +0400

    basic TS logic added

--- frontend/src/app/chat/dialogs/user-card-dialog/user-card-dialog.component.ts
+++ frontend/src/app/chat/dialogs/user-card-dialog/user-card-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-card-dialog',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './user-card-dialog.component.html',
   styleUrl: './user-card-dialog.component.scss',
 })
-export class UserCardDialogComponent {}
+export class UserCardDialogComponent {
+  constructor(
+    private dialogRef: MatDialogRef<UserCardDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {}
+  ) {}
+}
