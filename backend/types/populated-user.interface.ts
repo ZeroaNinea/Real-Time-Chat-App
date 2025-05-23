2591f1c9fc7eb1078dd0426cbd8742fda57eb098
Catgirl <zeroaninea@gmail.com>
2025-05-24 00:01:40 +0400

    question marks removed

--- backend/types/populated-user.interface.ts
+++ backend/types/populated-user.interface.ts
@@ -3,8 +3,8 @@ import { Types } from 'mongoose';
 export interface PopulatedUser {
   _id: Types.ObjectId;
   username: string;
-  avatar?: string;
-  bio?: string;
-  pronouns?: string;
-  status?: string;
+  avatar: string;
+  bio: string;
+  pronouns: string;
+  status: string;
 }
