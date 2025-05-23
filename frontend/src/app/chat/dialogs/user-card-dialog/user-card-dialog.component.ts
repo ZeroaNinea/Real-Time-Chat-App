a00bdfabcd251be46d57610e9267e78bbad5abd1
Catgirl <zeroaninea@gmail.com>
2025-05-23 21:41:22 +0400

    basic HTML added

--- frontend/src/app/chat/dialogs/user-card-dialog/user-card-dialog.component.ts
+++ frontend/src/app/chat/dialogs/user-card-dialog/user-card-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 import { PopulatedUser } from '../../shared/models/populated-user.model';
 
 @Component({
   selector: 'app-user-card-dialog',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule],
   standalone: true,
   templateUrl: './user-card-dialog.component.html',
   styleUrl: './user-card-dialog.component.scss',
