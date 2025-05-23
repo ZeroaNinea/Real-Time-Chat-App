463787d7640e3b6516f8ca874818c75616108ad0
Catgirl <zeroaninea@gmail.com>
2025-05-24 00:02:35 +0400

    bio and status added in populated-user.model.ts

--- frontend/src/app/chat/shared/models/populated-user.model.ts
+++ frontend/src/app/chat/shared/models/populated-user.model.ts
@@ -3,7 +3,9 @@ export interface PopulatedUser {
     _id: string;
     username: string;
     avatar: string;
+    bio: string;
     pronouns: string;
+    status: string;
   };
   roles: string[];
 }
