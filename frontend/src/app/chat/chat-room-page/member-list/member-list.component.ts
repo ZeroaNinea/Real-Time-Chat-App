1226c350f90b372038e3654b8767e10ee509430b
Catgirl <zeroaninea@gmail.com>
2025-05-24 14:36:26 +0400

    max specified for cutting

--- frontend/src/app/chat/chat-room-page/member-list/member-list.component.ts
+++ frontend/src/app/chat/chat-room-page/member-list/member-list.component.ts
@@ -35,7 +35,7 @@ export class MemberListComponent {
     });
   }
 
-  trimText(text: string, max = 20): string {
+  trimText(text: string, max: number): string {
     if (!text) return '';
     const trimmed = text.slice(0, max).trimEnd();
     return trimmed.endsWith('.') ? trimmed : trimmed + '...';
