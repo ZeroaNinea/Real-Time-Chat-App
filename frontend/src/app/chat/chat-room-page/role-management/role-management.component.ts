90447037c900f8bed1937cb0e2d26085aec7de2d
Catgirl <zeroaninea@gmail.com>
2025-05-26 20:00:45 +0400

    basic HTML added

--- frontend/src/app/chat/chat-room-page/role-management/role-management.component.ts
+++ frontend/src/app/chat/chat-room-page/role-management/role-management.component.ts
@@ -2,10 +2,21 @@ import { Component, Input } from '@angular/core';
 
 import { ChatRoomRole } from '../../shared/models/chat-room-roles.alias';
 import { PopulatedUser } from '../../shared/models/populated-user.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-role-management',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatListModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+  ],
   standalone: true,
   templateUrl: './role-management.component.html',
   styleUrl: './role-management.component.scss',
@@ -18,4 +29,10 @@ export class RoleManagementComponent {
   @Input() isAdmin = false;
   @Input() isOwner = false;
   @Input() isModerator = false;
+
+  role: Partial<ChatRoomRole> = {};
+
+  cancel() {
+    console.log('Cancel');
+  }
 }
