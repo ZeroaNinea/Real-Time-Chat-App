11808bdfba7280d933c200d31827e7c023d3ada5
Catgirl <zeroaninea@gmail.com>
2025-05-27 19:03:32 +0400

    *canDeleteChatRoom

--- frontend/src/app/chat/chat-room-page/role-management/role-management.component.ts
+++ frontend/src/app/chat/chat-room-page/role-management/role-management.component.ts
@@ -49,7 +49,7 @@ export class RoleManagementComponent {
     'canCreateChannels',
     'canEditChannels',
     'canDeleteChannels',
-    'candeleteChatroom',
+    'canDeleteChatroom',
     'canAssignRoles',
     'canAssignAdmins',
     'canAssignModerators',
