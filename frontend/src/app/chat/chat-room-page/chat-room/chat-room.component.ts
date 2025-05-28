95a0334c1a955824b9ca2fb86488020768a51a8a
Catgirl <zeroaninea@gmail.com>
2025-05-28 17:07:13 +0400

    the log removed in the chat-room.component.ts

--- frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
+++ frontend/src/app/chat/chat-room-page/chat-room/chat-room.component.ts
@@ -269,7 +269,6 @@ export class ChatRoomComponent implements OnDestroy {
 
     this.wsService.listenChatUpdates().subscribe((updatedChat) => {
       // Call the listener to update the frontend's data.
-      console.log('Chat updated:', updatedChat);
       this.chatName.set(updatedChat.name);
       this.members.set(updatedChat.members);
       this.chatRoomRoles.set(updatedChat.roles as ChatRoomRole[]);
