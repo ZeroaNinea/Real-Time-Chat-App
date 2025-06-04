1f88a46076eb83b4cf47d1209418dbf414939aaf
Catgirl <zeroaninea@gmail.com>
2025-06-04 06:33:23 +0400

    the ChatRoomListComponent created

---
+++ frontend/src/app/chat/main-page/chat-room-list/chat-room-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-room-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './chat-room-list.component.html',
+  styleUrl: './chat-room-list.component.scss',
+})
+export class ChatRoomListComponent {}
