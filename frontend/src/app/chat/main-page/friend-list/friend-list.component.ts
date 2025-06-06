0416c9a65d2ea8697a9e81068a0237d1c8c94e95
Catgirl <zeroaninea@gmail.com>
2025-06-06 19:58:52 +0400

    friend list component created

---
+++ frontend/src/app/chat/main-page/friend-list/friend-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './friend-list.component.html',
+  styleUrl: './friend-list.component.scss',
+})
+export class FriendListComponent {}
