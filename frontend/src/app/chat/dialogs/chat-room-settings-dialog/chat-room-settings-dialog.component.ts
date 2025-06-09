f957387206f21e2d9bc99d60fa9fe86ec930dfc5
Catgirl <zeroaninea@gmail.com>
2025-06-09 21:46:14 +0400

    onCancel and onSave methods added

--- frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
+++ frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
@@ -1,9 +1,12 @@
 import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-chat-room-settings-dialog',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule],
   standalone: true,
   templateUrl: './chat-room-settings-dialog.component.html',
   styleUrl: './chat-room-settings-dialog.component.scss',
@@ -12,6 +15,18 @@ export class ChatRoomSettingsDialogComponent {
   constructor(
     private dialogRef: MatDialogRef<ChatRoomSettingsDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
-    public data: {}
+    public data: {
+      name: string;
+      preview: string;
+      topic: string;
+    }
   ) {}
+
+  onCancel() {
+    this.dialogRef.close(null);
+  }
+
+  onSave() {
+    this.dialogRef.close(this.data);
+  }
 }
