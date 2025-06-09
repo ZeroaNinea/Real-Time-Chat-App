f1f7d0f1b3208f8fcc8ea527750012feec1a6f62
Catgirl <zeroaninea@gmail.com>
2025-06-10 00:45:58 +0400

    MatButtonModule imported

--- frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
+++ frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-chat-room-settings-dialog',
-  imports: [MatFormFieldModule, MatInputModule, FormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
   standalone: true,
   templateUrl: './chat-room-settings-dialog.component.html',
   styleUrl: './chat-room-settings-dialog.component.scss',
