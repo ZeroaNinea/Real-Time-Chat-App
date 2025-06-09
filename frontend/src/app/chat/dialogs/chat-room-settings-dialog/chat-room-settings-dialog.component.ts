4dd5878c112d87d042c74362f818a356f7fa0082
Catgirl <zeroaninea@gmail.com>
2025-06-09 21:19:59 +0400

    importing dialog stuff

--- frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
+++ frontend/src/app/chat/dialogs/chat-room-settings-dialog/chat-room-settings-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-chat-room-settings-dialog',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './chat-room-settings-dialog.component.html',
   styleUrl: './chat-room-settings-dialog.component.scss',
 })
-export class ChatRoomSettingsDialogComponent {}
+export class ChatRoomSettingsDialogComponent {
+  constructor(
+    private dialogRef: MatDialogRef<ChatRoomSettingsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {}
+  ) {}
+}
