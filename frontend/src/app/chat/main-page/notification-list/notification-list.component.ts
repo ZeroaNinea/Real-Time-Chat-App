a99caf097a59cf51d268ec503f15166c1349618a
Catgirl <zeroaninea@gmail.com>
2025-06-14 18:32:30 +0400

    the notification list refatorred

--- frontend/src/app/chat/main-page/notification-list/notification-list.component.ts
+++ frontend/src/app/chat/main-page/notification-list/notification-list.component.ts
@@ -1,13 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { Notification } from '../../shared/models/notification.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-notification-list',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    DatePipe,
+  ],
   standalone: true,
   templateUrl: './notification-list.component.html',
   styleUrl: './notification-list.component.scss',
 })
 export class NotificationListComponent {
   @Input() notifications: Notification[] = [];
+
+  acceptRequest(notification: Notification) {
+    // Emit event or call service to accept the friend request.
+    console.log('Accepted', notification);
+  }
+
+  declineRequest(notification: Notification) {
+    // Emit event or call service to decline the friend request.
+    console.log('Declined', notification);
+  }
 }
