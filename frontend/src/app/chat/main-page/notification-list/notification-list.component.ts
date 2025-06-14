e01c80b9b74f87f1b94fa31425f3e91485c9a674
Catgirl <zeroaninea@gmail.com>
2025-06-14 12:31:41 +0400

    the NotificationListComponent created

---
+++ frontend/src/app/chat/main-page/notification-list/notification-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './notification-list.component.html',
+  styleUrl: './notification-list.component.scss',
+})
+export class NotificationListComponent {}
