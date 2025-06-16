58d401f08fd884f71a9aaac89ff4be52dc233977
Catgirl <zeroaninea@gmail.com>
2025-06-16 20:28:21 +0400

    return AbbreviatedPopulatedUser from the backend

--- backend/src/controllers/friend.controller.ts
+++ backend/src/controllers/friend.controller.ts
@@ -5,14 +5,14 @@ export const getFriends = async (req: Request, res: Response) => {
   try {
     const userId = req.user._id;
 
-    const user = await User.findById(userId);
-
-    const friends = await user!.frinds.populate(
+    const user = await User.findById(userId).populate(
       'friends',
       'username avatar bio pronouns status friends banlist pendingRequests'
     );
 
-    res.json(friends.friends);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    res.json(user.friends);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
