258e1660784029f396ed78b61bd9b96df9a942c9
Catgirl <zeroaninea@gmail.com>
2025-06-16 22:36:27 +0400

    getBanList imported to the router

--- backend/src/routes/social.routes.ts
+++ backend/src/routes/social.routes.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 import { authMiddleware } from '../auth/auth.middleware';
 import { asyncRoute } from '../controllers/auth.controller';
-import { getFriends } from '../controllers/social.controller';
+import { getBanList, getFriends } from '../controllers/social.controller';
 
 const router = express.Router();
 
 router.get('/get-friends', authMiddleware, asyncRoute(getFriends));
+router.get('/get-ban-list', authMiddleware, asyncRoute(getBanList));
 
 export default router;
