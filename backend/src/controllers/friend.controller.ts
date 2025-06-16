526bf0bff3aad3ee3cd34100b7acf133bbee8a5c
Catgirl <zeroaninea@gmail.com>
2025-06-16 20:21:37 +0400

    return populated user

--- backend/src/controllers/friend.controller.ts
+++ backend/src/controllers/friend.controller.ts
@@ -6,7 +6,11 @@ export const getFriends = async (req: Request, res: Response) => {
     const userId = req.user._id;
 
     const user = await User.findById(userId);
-    const friends = await user!.frinds.populate('friends', 'username avatar');
+
+    const friends = await user!.frinds.populate(
+      'friends',
+      'username avatar bio pronouns status friends banlist pendingRequests'
+    );
 
     res.json(friends.friends);
   } catch (error) {
