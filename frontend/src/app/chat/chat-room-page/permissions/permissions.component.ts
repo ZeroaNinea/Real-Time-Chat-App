8f743fcab1d0cb509c022b148bd33cbf4488f4f9
Catgirl <zeroaninea@gmail.com>
2025-06-17 00:38:05 +0400

    tooltip added into the PermissionsComponent

--- frontend/src/app/chat/chat-room-page/permissions/permissions.component.ts
+++ frontend/src/app/chat/chat-room-page/permissions/permissions.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-permissions',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   standalone: true,
   templateUrl: './permissions.component.html',
   styleUrl: './permissions.component.scss',
