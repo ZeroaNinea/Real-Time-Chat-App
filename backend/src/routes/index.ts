57f5fb8173168b931b06701e96e8425d19e9c4b7
Catgirl <zeroaninea@gmail.com>
2025-06-16 19:47:46 +0400

    friendsRoutes -> friendRoutes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,7 +4,7 @@ import authRoutes from './auth.routes';
 import chatRoutes from './chat.routes';
 import messageRoutes from './message.routes';
 import notificationRoutes from './notification.routes';
-import friendRoutes from './friends.routes';
+import friendsRoutes from './friends.routes';
 
 const router = express.Router();
 
@@ -12,6 +12,6 @@ router.use('/auth', authRoutes);
 router.use('/chat', chatRoutes);
 router.use('/message', messageRoutes);
 router.use('/notification', notificationRoutes);
-router.use('/friends', friendRoutes);
+router.use('/friends', friendsRoutes);
 
 export default router;
