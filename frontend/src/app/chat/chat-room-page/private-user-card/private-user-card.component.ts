1655d5690dec1da96e68e9fcbca7fbeb39ea1cda
Catgirl <zeroaninea@gmail.com>
2025-06-19 23:21:37 +0400

    basic PrivateUserComponent created

--- frontend/src/app/chat/chat-room-page/private-user-card/private-user-card.component.ts
+++ frontend/src/app/chat/chat-room-page/private-user-card/private-user-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { PopulatedUser } from '../../shared/models/populated-user.model';
 import { environment } from '../../../../environments/environment';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-private-user-card',
-  imports: [],
+  imports: [MatIconModule, MatCardModule],
   standalone: true,
   templateUrl: './private-user-card.component.html',
   styleUrl: './private-user-card.component.scss',
@@ -15,7 +17,15 @@ export class PrivateUserCardComponent {
 
   environment = environment;
 
-  getAvatarUrl(userId: string): string {
+  get otherUser(): PopulatedUser | undefined {
+    return this.members.find((m) => m.user._id !== this.currentUserId);
+  }
+
+  copyId(userId: string | undefined): void {
+    navigator.clipboard.writeText(userId || '');
+  }
+
+  getAvatarUrl(userId: string | undefined): string {
     const avatar = this.members.find((m) => m.user._id === userId)?.user.avatar;
     return avatar
       ? `${this.environment.backendUrl}/${avatar}`
