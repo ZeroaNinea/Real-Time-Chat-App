46e62afc4bb075e216a354529e85fa5aba0a29af
Catgirl <zeroaninea@gmail.com>
2025-06-21 23:13:17 +0400

    renamed to .handlers

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from 'socket.io';
-import { registerMessageHandlers } from './mesageHandlers';
-import { registerChannelHandlers } from './channelHandlers';
+import { registerMessageHandlers } from './mesage.handlers';
+import { registerChannelHandlers } from './channel.handlers';
 
 export function registerSocketHandlers(io: Server, socket: Socket) {
   registerMessageHandlers(io, socket);
