863b40865bec4b8fce609734c118be8dd2a4363e
Catgirl <zeroaninea@gmail.com>
2025-06-21 23:18:06 +0400

    registerAuthHandlers imported into the index.ts

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,8 +1,11 @@
 import { Server, Socket } from 'socket.io';
+
 import { registerMessageHandlers } from './mesage.handlers';
 import { registerChannelHandlers } from './channel.handlers';
+import { registerAuthHandlers } from './auth.handlers';
 
 export function registerSocketHandlers(io: Server, socket: Socket) {
   registerMessageHandlers(io, socket);
   registerChannelHandlers(io, socket);
+  registerAuthHandlers(io, socket);
 }
