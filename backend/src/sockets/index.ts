f73846ffcf7e7f8529977cfb6336f1a5b08bd6cf
Catgirl <zeroaninea@gmail.com>
2025-06-21 22:29:17 +0400

    registerMessageHandlers added into the index.ts

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,3 +1,6 @@
 import { Server, Socket } from 'socket.io';
+import { registerMessageHandlers } from './mesageHandlers';
 
-export function registerSocketHandlers(io: Server, socket: Socket) {}
+export function registerSocketHandlers(io: Server, socket: Socket) {
+  registerMessageHandlers(io, socket);
+}
