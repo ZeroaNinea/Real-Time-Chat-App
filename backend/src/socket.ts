c3ea545d948b7f4b0be385cbdb5fb71fcc9ea542
Catgirl <zeroaninea@gmail.com>
2025-06-21 23:44:56 +0400

    sendFriendRequest moved

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -94,62 +94,6 @@ export function setupSocket(server: HttpServer, app: Express) {
       socket.join(channelRoom);
     });
 
-    socket.on('sendFriendRequest', async ({ receiverId }, callback) => {
-      try {
-        const senderId = socket.data.user._id.toString();
-        const sender = await User.findById(senderId);
-        const receiver = await User.findById(receiverId);
-
-        if (!sender || !receiver)
-          return callback?.({ error: 'User not found' });
-
-        if (receiver.friends?.includes(senderId))
-          return callback?.({ error: 'Already friends' });
-
-        if (sender.pendingRequests?.includes(receiverId))
-          return callback?.({ error: 'Friend request already sent' });
-
-        if (senderId === receiverId)
-          return callback?.({
-            error: 'Cannot send friend request to yourself',
-          });
-
-        if (receiver.banlist?.includes(senderId))
-          return callback?.({ error: 'User is banned' });
-
-        if (sender.banlist?.includes(receiverId))
-          return callback?.({ error: 'You are banned by the user' });
-
-        sender.pendingRequests?.push(receiverId);
-        await sender.save();
-
-        const notification = new Notification({
-          sender: senderId,
-          recipient: receiverId,
-          type: 'friend-request',
-          message: `${sender.username} sent you a friend request`,
-          link: '/friends',
-        });
-
-        await notification.save();
-
-        const targetSocketId = receiver._id.toString();
-
-        if (targetSocketId) {
-          const populatedNotification = await notification.populate(
-            'sender',
-            'username avatar'
-          );
-          io.to(targetSocketId).emit('notification', populatedNotification);
-        }
-
-        callback?.({ success: true });
-      } catch (err) {
-        console.error(err);
-        callback?.({ error: 'Server error' });
-      }
-    });
-
     socket.on(
       'acceptFriendRequest',
       async ({ notificationId, senderId }, callback) => {
