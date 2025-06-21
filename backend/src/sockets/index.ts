a1444594ec5a0beafe5aea56498e30ffa38c1f7e
Catgirl <zeroaninea@gmail.com>
2025-06-21 23:01:40 +0400

    registerChannelHandlers added into the index.ts

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,6 +1,8 @@
 import { Server, Socket } from 'socket.io';
 import { registerMessageHandlers } from './mesageHandlers';
+import { registerChannelHandlers } from './channelHandlers';
 
 export function registerSocketHandlers(io: Server, socket: Socket) {
   registerMessageHandlers(io, socket);
+  registerChannelHandlers(io, socket);
 }
