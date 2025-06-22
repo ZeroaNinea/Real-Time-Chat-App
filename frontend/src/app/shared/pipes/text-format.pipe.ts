38c15a244e2f8bc60508ec481fc2a382a5770862
Catgirl <zeroaninea@gmail.com>
2025-06-22 18:10:13 +0400

    character escaping added into the TextFormatPipe

--- frontend/src/app/shared/pipes/text-format.pipe.ts
+++ frontend/src/app/shared/pipes/text-format.pipe.ts
@@ -6,10 +6,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TextFormatPipe implements PipeTransform {
   transform(text: string): string {
     if (!text) return '';
+
+    // Escape markers.
+    text = text
+      .replace(/\\\*/g, '__ESC_STAR__')
+      .replace(/\\\_/g, '__ESC_UNDERSCORE__')
+      .replace(/\\\~/g, '__ESC_TILDE__');
+
+    // Format.
+    text = text
+      .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Bold.
+      .replace(/\*(.*?)\*/g, '<em>$1</em>') // Italic.
+      .replace(/\_\_(.*?)\_\_/g, '<u>$1</u>') // Underline.
+      .replace(/\~\~(.*?)\~\~/g, '<s>$1</s>'); // Strikethrough.
+
+    // Restore escaped markers.
     return text
-      .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
-      .replace(/\*(.*?)\*/g, '<em>$1</em>')
-      .replace(/\_\_(.*?)\_\_/g, '<u>$1</u>')
-      .replace(/\~\~(.*?)\~\~/g, '<s>$1</s>');
+      .replace(/__ESC_STAR__/g, '*')
+      .replace(/__ESC_UNDERSCORE__/g, '_')
+      .replace(/__ESC_TILDE__/g, '~');
   }
 }
