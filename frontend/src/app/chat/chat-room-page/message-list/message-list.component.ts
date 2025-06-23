909f8c33bcdb9c422caec445b6ede532d434abfb
Catgirl <zeroaninea@gmail.com>
2025-06-23 19:53:48 +0400

    the old scroll to the message code removed

--- frontend/src/app/chat/chat-room-page/message-list/message-list.component.ts
+++ frontend/src/app/chat/chat-room-page/message-list/message-list.component.ts
@@ -183,12 +183,6 @@ export class MessageListComponent {
   }
 
   scrollToMessage(messageId: string, attempt = 0): void {
-    // const element = document.getElementById(`message-${messageId}`);
-    // if (element) {
-    //   element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    //   element.classList.add('highlighted');
-    //   setTimeout(() => element.classList.remove('highlighted'), 2000);
-    // }
     const maxAttempts = 10;
 
     const el = document.getElementById(`message-${messageId}`);
