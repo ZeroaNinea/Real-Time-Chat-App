7b2b182255108050d88aa991afdf9d0de350b843
Catgirl <zeroaninea@gmail.com>
2025-06-25 17:18:48 +0400

    GIF methods added

--- frontend/src/app/shared/components/gif-picker/gif-picker.component.ts
+++ frontend/src/app/shared/components/gif-picker/gif-picker.component.ts
@@ -16,4 +16,25 @@ export class GifPickerComponent {
 
   searchTerm = '';
   gifs: string[] = [];
+
+  constructor() {
+    this.loadTrending();
+  }
+
+  loadTrending() {
+    this.gifService.trendingGifs().subscribe((res) => {
+      this.gifs = res.results.map((r) => r.media_formats.gif.url);
+    });
+  }
+
+  search() {
+    if (!this.searchTerm.trim()) return this.loadTrending();
+    this.gifService.searchGifs(this.searchTerm).subscribe((res) => {
+      this.gifs = res.results.map((r) => r.media_formats.gif.url);
+    });
+  }
+
+  selectGif(url: string) {
+    this.select.emit(url);
+  }
 }
