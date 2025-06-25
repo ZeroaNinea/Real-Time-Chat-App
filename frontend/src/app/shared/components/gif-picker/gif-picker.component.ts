6aabf397c099088b88102be131b3a9a0b7018de9
Catgirl <zeroaninea@gmail.com>
2025-06-25 16:30:39 +0400

    the GifPickerComponent created

---
+++ frontend/src/app/shared/components/gif-picker/gif-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-picker',
+  imports: [],
+  standalone: true,
+  templateUrl: './gif-picker.component.html',
+  styleUrl: './gif-picker.component.scss',
+})
+export class GifPickerComponent {}
