d24989a2762bc5456caeee554eeabb888dc7ab04
Catgirl <zeroaninea@gmail.com>
2025-06-25 16:33:33 +0400

    writing the GifPickerComponent's logic

--- frontend/src/app/shared/components/gif-picker/gif-picker.component.ts
+++ frontend/src/app/shared/components/gif-picker/gif-picker.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { GifService } from '../../services/gif/gif.service';
 
 @Component({
   selector: 'app-gif-picker',
-  imports: [],
+  imports: [FormsModule],
   standalone: true,
   templateUrl: './gif-picker.component.html',
   styleUrl: './gif-picker.component.scss',
 })
-export class GifPickerComponent {}
+export class GifPickerComponent {
+  private gifService = inject(GifService);
+
+  @Output() select = new EventEmitter<string>();
+
+  searchTerm = '';
+  gifs: string[] = [];
+}
