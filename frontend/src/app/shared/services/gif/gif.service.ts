3b2f84f2f77cc18ff7b5d9aa4419dfa99864655b
Catgirl <zeroaninea@gmail.com>
2025-06-25 15:54:53 +0400

    api key added into the environments

--- frontend/src/app/shared/services/gif/gif.service.ts
+++ frontend/src/app/shared/services/gif/gif.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifService {
-  private readonly apiKey = ' AIzaSyCeUgqzaZhxMmo_mgpkeXbXgFAfm8zoAX4 ';
+  environment = environment;
+
+  private readonly apiKey = environment.tenorApiKey;
   private readonly baseUrl = 'https://tenor.googleapis.com/v2';
 
   private http: HttpClient = inject(HttpClient);
