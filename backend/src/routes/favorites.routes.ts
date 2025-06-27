a4e1aa41c38e7634902f3c2c6286292ebae7dfb7
Catgirl <zeroaninea@gmail.com>
2025-06-27 19:34:08 +0400

    ggetFavoriteGifs added into the router

--- backend/src/routes/favorites.routes.ts
+++ backend/src/routes/favorites.routes.ts
@@ -2,7 +2,10 @@ import express from 'express';
 
 import { authMiddleware } from '../auth/auth.middleware';
 import { asyncRoute } from '../controllers/auth.controller';
+import { getFavoriteGifs } from '../controllers/favorites.controller';
 
 const router = express.Router();
+
+router.get('/get-favorites', authMiddleware, asyncRoute(getFavoriteGifs));
 
 export default router;
