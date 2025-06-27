165cfa3004ca09a888d391b70f91acba843e6373
Catgirl <zeroaninea@gmail.com>
2025-06-27 19:48:17 +0400

    removeFavorite added into the controller

--- backend/src/routes/favorites.routes.ts
+++ backend/src/routes/favorites.routes.ts
@@ -2,10 +2,14 @@ import express from 'express';
 
 import { authMiddleware } from '../auth/auth.middleware';
 import { asyncRoute } from '../controllers/auth.controller';
-import { getFavorites } from '../controllers/favorites.controller';
+import {
+  getFavorites,
+  removeFavorite,
+} from '../controllers/favorites.controller';
 
 const router = express.Router();
 
 router.get('/get-favorites', authMiddleware, asyncRoute(getFavorites));
+router.delete('/remove-favorite', authMiddleware, asyncRoute(removeFavorite));
 
 export default router;
