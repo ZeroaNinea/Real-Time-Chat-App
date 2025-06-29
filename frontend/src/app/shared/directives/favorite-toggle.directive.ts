731a2ab869589f6e8351bc9a80fdbde6b2516098
Catgirl <zeroaninea@gmail.com>
2025-06-30 01:34:15 +0400

    the logic added

--- frontend/src/app/shared/directives/favorite-toggle.directive.ts
+++ frontend/src/app/shared/directives/favorite-toggle.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, inject, Input, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  Input,
+  Renderer2,
+} from '@angular/core';
 import { ChatService } from '../../chat/shared/services/chat-service/chat.service';
 
 @Directive({
@@ -18,5 +25,43 @@ export class FavoriteToggleDirective {
     this.chatService.favorites$.subscribe((favs) => {
       this.favoriteGifs = favs;
     });
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    event.stopPropagation();
+
+    const wrapper = this.el.nativeElement.closest('.marked-star-wrapper');
+    const particleContainer = wrapper?.querySelector(
+      '.marked-particle-container'
+    ) as HTMLElement;
+
+    if (particleContainer) {
+      this.animateParticles(particleContainer);
+    }
+
+    if (this.favoriteGifs.includes(this.gifUrl)) {
+      this.chatService.removeFavorite(this.gifUrl).subscribe();
+    } else {
+      this.chatService.addFavorite(this.gifUrl).subscribe();
+    }
+  }
+
+  animateParticles(container: Element) {
+    for (let i = 0; i < 12; i++) {
+      const particle = this.renderer.createElement('div');
+      this.renderer.addClass(particle, 'particle');
+
+      this.renderer.setStyle(particle, '--x', `${Math.random() * 2 - 1}`);
+      this.renderer.setStyle(particle, '--y', `${Math.random() * 2 - 1}`);
+      this.renderer.setStyle(particle, 'width', `6px`);
+      this.renderer.setStyle(particle, 'height', `6px`);
+
+      this.renderer.appendChild(container, particle);
+
+      setTimeout(() => {
+        this.renderer.removeChild(container, particle);
+      }, 600);
+    }
   }
 }
