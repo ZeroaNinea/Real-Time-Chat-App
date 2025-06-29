0228d8352a029a638c453e7b361d095bc4c63e88
Catgirl <zeroaninea@gmail.com>
2025-06-30 01:55:34 +0400

    the directive removed

--- frontend/src/app/shared/directives/favorite-toggle.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FavoriteToggleDirective } from './favorite-toggle.directive';
-
-describe('FavoriteToggleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FavoriteToggleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
