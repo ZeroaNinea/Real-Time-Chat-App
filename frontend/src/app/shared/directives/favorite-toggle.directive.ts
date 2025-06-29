b20ad51d67ca3a15fec387708876c9d502533c6e
Catgirl <zeroaninea@gmail.com>
2025-06-30 01:25:32 +0400

    main properties added

--- frontend/src/app/shared/directives/favorite-toggle.directive.ts
+++ frontend/src/app/shared/directives/favorite-toggle.directive.ts
@@ -1,9 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, Input, Renderer2 } from '@angular/core';
+import { ChatService } from '../../chat/shared/services/chat-service/chat.service';
 
 @Directive({
   selector: '[appFavoriteToggle]',
   standalone: true,
 })
 export class FavoriteToggleDirective {
+  private el = inject(ElementRef<HTMLElement>);
+  private renderer = inject(Renderer2);
+  private chatService = inject(ChatService);
+
+  @Input('appFavoriteToggle') gifUrl!: string;
+
+  favoritesGifs: string[] = [];
+
   constructor() {}
 }
