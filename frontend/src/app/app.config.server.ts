bfdd8e78620d4e70d3d464a23bf2c78674d8d20c
Catgirl <zeroaninea@gmail.com>
2025-07-06 04:49:41 +0400

    still cries

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -1,14 +1,20 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
+import { provideServerRouting, ServerRoute } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
-  ]
+    provideServerRouting(serverRoutes),
+    // provideServerRouting(serverRoutes, {
+    //   renderMode: (route: ServerRoute) =>
+    //     route.path?.includes(':chatId') || route.path?.includes(':channelId')
+    //       ? 'no-prerender'
+    //       : undefined,
+    // } as any),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
