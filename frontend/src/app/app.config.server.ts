e1e92d65e3beb360b2b9baa1ef2fa933b536a04e
Catgirl <zeroaninea@gmail.com>
2025-07-06 15:30:00 +0400

    updated to the 20

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -1,11 +1,10 @@
+import { ServerRoute, provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting, ServerRoute } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
+  providers: [provideServerRendering(withRoutes(serverRoutes))],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
