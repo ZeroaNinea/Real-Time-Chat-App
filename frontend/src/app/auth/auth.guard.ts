dacbb84cb980eb508f8b6d508b31eb5f8e6f0134
Catgirl <zeroaninea@gmail.com>
2025-07-09 14:30:53 +0400

    not isPlatformBrowser

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,8 +1,12 @@
 import { CanActivateFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const platformId = inject(PLATFORM_ID);
   const router = inject(Router);
+
+  if (!isPlatformBrowser(platformId)) return false;
 
   if (typeof window === 'undefined') {
     // Can't check tokens server-side.
