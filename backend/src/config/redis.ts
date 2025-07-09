64a356ed7e7b30beb43287538c8c8f5a160a51cd
Catgirl <zeroaninea@gmail.com>
2025-07-09 18:54:47 +0400

    switch to the cloud databases

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -3,14 +3,14 @@ import config from './env';
 
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, UPSTASH_REDIS_URL } = config;
 
-const redisConfig = {
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD,
-  db: 0,
-};
+// const redisConfig = {
+//   host: REDIS_HOST,
+//   port: REDIS_PORT,
+//   password: REDIS_PASSWORD,
+//   db: 0,
+// };
 
-const redis = new Redis(redisConfig);
+const redis = new Redis(UPSTASH_REDIS_URL);
 const redisClient = redis.duplicate();
 
 redisClient.on('error', (err) => {
