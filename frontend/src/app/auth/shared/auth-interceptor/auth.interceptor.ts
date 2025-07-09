b0a3beddf7209997f552ff71161ed4c8336d55e2
Catgirl <zeroaninea@gmail.com>
2025-07-09 18:17:20 +0400

    skip ssr in the interceptor

--- frontend/src/app/auth/shared/auth-interceptor/auth.interceptor.ts
+++ frontend/src/app/auth/shared/auth-interceptor/auth.interceptor.ts
@@ -1,6 +1,14 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) {
+    return next(req);
+  }
+
   if (typeof window === 'undefined') return next(req);
 
   const token = localStorage.getItem('accessToken');
