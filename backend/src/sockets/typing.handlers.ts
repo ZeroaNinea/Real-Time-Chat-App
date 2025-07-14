a71f9d954114a87d46e70b694013c2709863b6de
Catgirl <zeroaninea@gmail.com>
2025-07-14 22:03:20 +0400

    logs for typing added

--- backend/src/sockets/typing.handlers.ts
+++ backend/src/sockets/typing.handlers.ts
@@ -2,6 +2,7 @@ import { Server, Socket } from 'socket.io';
 
 export function registerTypingHandlers(io: Server, socket: Socket) {
   socket.on('typingStart', ({ chatId, channelId }) => {
+    console.log('typingStart', { chatId, channelId });
     const channelRoom = `${chatId}:${channelId}`;
     socket.to(channelRoom).emit('userTypingStart', {
       userId: socket.data.user._id,
@@ -11,6 +12,7 @@ export function registerTypingHandlers(io: Server, socket: Socket) {
   });
 
   socket.on('typingStop', ({ chatId, channelId }) => {
+    console.log('typingStop', { chatId, channelId });
     const channelRoom = `${chatId}:${channelId}`;
     socket.to(channelRoom).emit('userTypingStop', {
       userId: socket.data.user._id,
