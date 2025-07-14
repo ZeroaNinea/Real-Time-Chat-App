302db115aaa257c5422ca2680322ffc1ea79dfa5
Catgirl <zeroaninea@gmail.com>
2025-07-14 20:43:43 +0400

    signal removed

--- frontend/src/app/chat/chat-room-page/message-input/message-input.component.ts
+++ frontend/src/app/chat/chat-room-page/message-input/message-input.component.ts
@@ -6,7 +6,6 @@ import {
   Input,
   Output,
   ViewChild,
-  signal,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
