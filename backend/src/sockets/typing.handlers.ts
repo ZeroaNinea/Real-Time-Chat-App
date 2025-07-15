e2c2c420c1cbf7bcbdffb102e66081f608453dd2
Catgirl <zeroaninea@gmail.com>
2025-07-15 11:59:58 +0400

    logs removed

--- backend/src/sockets/typing.handlers.ts
+++ backend/src/sockets/typing.handlers.ts
@@ -2,7 +2,6 @@ import { Server, Socket } from 'socket.io';
 
 export function registerTypingHandlers(io: Server, socket: Socket) {
   socket.on('typingStart', ({ chatId, channelId }) => {
-    console.log('typingStart', { chatId, channelId });
     const channelRoom = `${chatId}:${channelId}`;
 
     io.to(channelRoom).emit('userTypingStart', {
@@ -13,7 +12,6 @@ export function registerTypingHandlers(io: Server, socket: Socket) {
   });
 
   socket.on('typingStop', ({ chatId, channelId }) => {
-    console.log('typingStop', { chatId, channelId });
     const channelRoom = `${chatId}:${channelId}`;
 
     io.to(channelRoom).emit('userTypingStop', {
