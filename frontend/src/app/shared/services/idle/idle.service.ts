859f6493ccd0147697438f476659d8ab5cd8b156
Catgirl <zeroaninea@gmail.com>
2025-07-16 17:53:27 +0400

    idle.service.ts created

---
+++ frontend/src/app/shared/services/idle/idle.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { WebsocketService } from '../../../chat/shared/services/websocket/websocket.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IdleService {
+  constructor() {}
+
+  private timeout: any;
+  private delay = 60000; // 1 minute
+
+  init(wsService: WebsocketService) {
+    const reset = () => {
+      clearTimeout(this.timeout);
+      wsService.emitUserActive();
+      this.timeout = setTimeout(() => {
+        wsService.emitUserIdle();
+      }, this.delay);
+    };
+
+    ['mousemove', 'keydown', 'click', 'scroll'].forEach((event) =>
+      window.addEventListener(event, reset)
+    );
+
+    reset();
+  }
+}
