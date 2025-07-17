eb96b099580ec31a40474826c348a8414aa8ea77
Catgirl <zeroaninea@gmail.com>
2025-07-17 11:51:49 +0400

    check if a member

--- backend/src/sockets/reaction.handlers.ts
+++ backend/src/sockets/reaction.handlers.ts
@@ -1,6 +1,8 @@
 import { Socket, Server } from 'socket.io';
 import { Message } from '../models/message.model';
 import { Reaction } from '../../types/reaction.alias';
+import { Chat } from '../models/chat.model';
+import { Member } from '../../types/member.alias';
 
 export function registerReactionHandlers(io: Server, socket: Socket) {
   socket.on('toggleReaction', ({ chatId, messageId, reaction }, callback) => {
@@ -13,6 +15,17 @@ export function registerReactionHandlers(io: Server, socket: Socket) {
     const message = Message.findById(messageId);
     if (!message) {
       return callback?.({ error: 'Message not found' });
+    }
+
+    const currentChatRoom = Chat.findById(chatId);
+    if (!currentChatRoom) {
+      return callback?.({ error: 'Chat room not found' });
+    }
+
+    if (
+      !currentChatRoom.members.some((m: Member) => m.user.equals(currentUserId))
+    ) {
+      return callback?.({ error: 'You are not a member of this chat room' });
     }
 
     if (message.reactions.some((r: Reaction) => r.user.equals(currentUserId))) {
