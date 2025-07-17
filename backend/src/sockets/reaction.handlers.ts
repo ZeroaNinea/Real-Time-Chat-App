d76d47dc51970c3353e1940e8e3113bc0c06a185
Catgirl <zeroaninea@gmail.com>
2025-07-17 11:45:56 +0400

    toggle logic added

--- backend/src/sockets/reaction.handlers.ts
+++ backend/src/sockets/reaction.handlers.ts
@@ -1,5 +1,6 @@
 import { Socket, Server } from 'socket.io';
 import { Message } from '../models/message.model';
+import { Reaction } from '../../types/reaction.alias';
 
 export function registerReactionHandlers(io: Server, socket: Socket) {
   socket.on('toggleReaction', ({ chatId, messageId, reaction }, callback) => {
@@ -12,6 +13,14 @@ export function registerReactionHandlers(io: Server, socket: Socket) {
     const message = Message.findById(messageId);
     if (!message) {
       return callback?.({ error: 'Message not found' });
+    }
+
+    if (message.reactions.some((r: Reaction) => r.user.equals(currentUserId))) {
+      message.reactions = message.reactions.filter(
+        (r: Reaction) => !r.user.equals(currentUserId)
+      );
+    } else {
+      message.reactions.push({ emoji: reaction, user: currentUserId });
     }
 
     io.to(chatId).emit('reactionToggled', message);
