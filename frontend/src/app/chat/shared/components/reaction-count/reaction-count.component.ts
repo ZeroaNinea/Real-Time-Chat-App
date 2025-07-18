b1f4ed60f9175a6f70c410af82b2d5da4dfb6118
Catgirl <zeroaninea@gmail.com>
2025-07-18 23:49:40 +0400

    cries very bad

--- frontend/src/app/chat/shared/components/reaction-count/reaction-count.component.ts
+++ frontend/src/app/chat/shared/components/reaction-count/reaction-count.component.ts
@@ -1,11 +1,11 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import {
-  afterNextRender,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-reaction-count',
@@ -14,12 +14,20 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './reaction-count.component.html',
   styleUrl: './reaction-count.component.scss',
   animations: [
-    trigger('bump', [
-      transition(':enter', []),
-      transition('* => true', [
-        style({ transform: 'scale(1)' }),
-        animate('150ms ease', style({ transform: 'scale(1.3)' })),
-        animate('150ms ease', style({ transform: 'scale(1)' })),
+    trigger('countChange', [
+      transition(':increment', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(
+          '200ms ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate(
+          '200ms ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
       ]),
     ]),
   ],
@@ -29,12 +37,14 @@ export class ReactionCountComponent implements OnChanges {
   @Input() emoji = '';
   @Input() shouldAnimate = false;
 
-  animationState = false;
+  animationState: 'active' | 'inactive' = 'inactive';
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['shouldAnimate'] && this.shouldAnimate) {
-      this.animationState = true;
-      setTimeout(() => (this.animationState = false), 300);
+      this.animationState = 'active';
+      setTimeout(() => {
+        this.animationState = 'inactive';
+      }, 300);
     }
   }
 }
