197c1aa54648baa751fee291db224a4837ebde6d
Catgirl <zeroaninea@gmail.com>
2025-07-18 22:22:32 +0400

    TS added

--- frontend/src/app/chat/shared/components/reaction-count/reaction-count.component.ts
+++ frontend/src/app/chat/shared/components/reaction-count/reaction-count.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-reaction-count',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   standalone: true,
   templateUrl: './reaction-count.component.html',
   styleUrl: './reaction-count.component.scss',
+  animations: [
+    trigger('slideUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(
+          '150ms ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '150ms ease-in',
+          style({ transform: 'translateY(-100%)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class ReactionCountComponent {}
+export class ReactionCountComponent implements OnChanges {
+  @Input() count = 0;
+  previousCount = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['count'] && !changes['count'].firstChange) {
+      this.previousCount = changes['count'].previousValue;
+    }
+  }
+}
