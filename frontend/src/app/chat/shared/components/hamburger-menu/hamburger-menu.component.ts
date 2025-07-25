843cff823f55e28d26d60b977c3d555f83509a7c
Catgirl <zeroaninea@gmail.com>
2025-07-25 23:59:19 +0400

    HamburgerMenuComponent created

---
+++ frontend/src/app/chat/shared/components/hamburger-menu/hamburger-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger-menu',
+  imports: [],
+  standalone: true,
+  templateUrl: './hamburger-menu.component.html',
+  styleUrl: './hamburger-menu.component.scss',
+})
+export class HamburgerMenuComponent {}
