4ef8d49b3e5c95a5f68239a53990ba19797b6edd
Catgirl <zeroaninea@gmail.com>
2025-07-26 00:01:56 +0400

    RouterOutlet imported

--- frontend/src/app/chat/shared/components/hamburger-menu/hamburger-menu.component.ts
+++ frontend/src/app/chat/shared/components/hamburger-menu/hamburger-menu.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-hamburger-menu',
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule, RouterOutlet],
   standalone: true,
   templateUrl: './hamburger-menu.component.html',
   styleUrl: './hamburger-menu.component.scss',
