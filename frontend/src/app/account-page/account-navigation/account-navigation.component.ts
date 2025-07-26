e76d6e7dfa9aa8c5ffb7f693289338a415fb9007
Catgirl <zeroaninea@gmail.com>
2025-07-26 18:15:08 +0400

    AccountNavigationComponent created

---
+++ frontend/src/app/account-page/account-navigation/account-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-navigation',
+  imports: [],
+  standalone: true,
+  templateUrl: './account-navigation.component.html',
+  styleUrl: './account-navigation.component.scss',
+})
+export class AccountNavigationComponent {}
