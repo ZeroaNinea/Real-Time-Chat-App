54667ca3f1c0a6814527e3efeb1d97cfe23f1ddd
Catgirl <zeroaninea@gmail.com>
2025-07-26 18:24:39 +0400

    HTML added

--- frontend/src/app/account-page/account-navigation/account-navigation.component.ts
+++ frontend/src/app/account-page/account-navigation/account-navigation.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-account-navigation',
-  imports: [],
+  imports: [RouterModule, MatButtonModule, MatIconModule],
   standalone: true,
   templateUrl: './account-navigation.component.html',
   styleUrl: './account-navigation.component.scss',
