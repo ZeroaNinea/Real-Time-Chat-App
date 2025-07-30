08516ce1780a6bd14c72a96bea812637e40f588c
Catgirl <zeroaninea@gmail.com>
2025-07-30 16:16:16 +0400

    HeaderComponent imported

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { HeaderComponent } from '../chat/main-page/header/header.component';
+
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, HeaderComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
