dfc709661a2ac5e03a26122b1c5ad90011bddb45
Catgirl <zeroaninea@gmail.com>
2025-07-31 20:04:48 +0400

    Welcome message added

--- frontend/src/app/landing-page/home/home.component.ts
+++ frontend/src/app/landing-page/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { LandingHeaderComponent } from '../landing-header/landing-header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, LandingHeaderComponent],
+  imports: [RouterLink, LandingHeaderComponent, MatButtonModule],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
