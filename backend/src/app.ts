35833cdea94d38500eed8ef8a43500ea96c37aeb
Catgirl <zeroaninea@gmail.com>
2025-08-05 21:39:47 +0400

    app.ts covered

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,21 +4,13 @@ import cors from 'cors';
 import router from './routes';
 import path from 'path';
 
+import { corsOriginValidator } from './helpers/cors-config';
+
 const app = express();
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      const allowed = [
-        'http://localhost:4200',
-        'https://real-time-chat-app.local',
-      ];
-      if (!origin || allowed.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: corsOriginValidator,
     credentials: true,
   })
 );
