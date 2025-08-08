79faed5610c5201e70041e10a72b1bf11b121010
Catgirl <zeroaninea@gmail.com>
2025-08-08 18:48:24 +0400

    should create a new account test added

--- backend/test/auth.controller.spec.ts
+++ backend/test/auth.controller.spec.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import request from 'supertest';
 
 import { connectToDatabase } from './helpers/connect-disconnect-db';
+import { app } from '../src/app';
 
 describe('Auth Controller', () => {
   let connectStub!: sinon.SinonStub;
@@ -30,7 +32,13 @@ describe('Auth Controller', () => {
     process.env = originalEnv;
   });
 
-  it('should create a new account', () => {
-    expect(true).to.be.true;
+  it('should create a new account', async () => {
+    const res = await request(app).post('/api/auth/register').send({
+      username: 'newuser',
+      email: 'newuser@email.com',
+      password: '123',
+    });
+
+    expect(res.status).to.equal(201);
   });
 });
