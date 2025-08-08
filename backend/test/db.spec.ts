7284964a82dba0c137f60e3430aba9741366690c
Catgirl <zeroaninea@gmail.com>
2025-08-08 18:35:52 +0400

    comments removed

--- backend/test/db.spec.ts
+++ backend/test/db.spec.ts
@@ -12,40 +12,6 @@ describe('Database Connection', () => {
   const originalEnv = process.env;
 
   beforeEach(() => {
-    // process.env = { ...originalEnv };
-    // connectStub = sinon.stub().resolves();
-    // disconnectStub = sinon.stub().resolves();
-
-    // // Stub MongoMemoryServer.
-    // const mockUri = 'mongodb://localhost:27017/in-memory-test';
-    // mongoMemoryStub = {
-    //   create: sinon.stub().resolves({
-    //     getUri: () => mockUri,
-    //     stop: sinon.stub().resolves(),
-    //   }),
-    // };
-
-    // dbModule = proxyquire('../src/config/db', {
-    //   mongoose: {
-    //     connect: connectStub,
-    //     disconnect: disconnectStub,
-    //     default: {}, // export default mongoose.
-    //   },
-    //   './env': {
-    //     default: {
-    //       NODE_ENV: 'test',
-    //       DB_USER: 'user',
-    //       DB_PASSWORD: 'pass',
-    //       DB_HOST: 'host',
-    //       DB_PORT: 1234,
-    //       DB_NAME: 'db',
-    //     },
-    //   },
-    //   'mongodb-memory-server': {
-    //     MongoMemoryServer: mongoMemoryStub,
-    //   },
-    // });
-
     const connection = connectToDatabase(
       connectStub,
       disconnectStub,
