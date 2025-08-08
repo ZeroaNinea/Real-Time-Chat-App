849ef908b64cc534b6faf99fa39807bacea1ff5b
Catgirl <zeroaninea@gmail.com>
2025-08-08 23:09:05 +0400

    cries very sad

--- backend/test/auth.controller.spec.ts
+++ backend/test/auth.controller.spec.ts
@@ -119,23 +119,90 @@ describe('Auth Controller', () => {
     expect(res.body.message).to.equal('Password is required.');
   });
 
-  it('should delete the account /api/auth/delete-account', async () => {
+  // it('should return 404 if user not found /api/auth/delete-account', async () => {
+  //   const stub = sinon.stub(User, 'findById').resolves(null);
+
+  //   const resLogin = await request(app).post('/api/auth/login').send({
+  //     username: 'newuser',
+  //     password: '123',
+  //   });
+
+  //   const res = await request(app)
+  //     .delete('/api/auth/delete-account')
+  //     .set('Authorization', `Bearer ${resLogin.body.token}`)
+  //     .send({ password: '123' });
+
+  //   console.log(res.body, '=========================');
+
+  //   expect(res.status).to.equal(404);
+  //   expect(res.body.message).to.equal('User not found.');
+
+  //   stub.restore();
+  // });
+
+  it('should return 404 if user not found /api/auth/delete-account', async () => {
+    const fakeUser = new User({
+      username: 'newuser',
+      email: 'newuser@email.com',
+      password: '123',
+    });
+
+    const stub = sinon.stub(User, 'findById');
+    stub.onFirstCall().resolves(fakeUser);
+    stub.onSecondCall().resolves(null);
+
     const resLogin = await request(app).post('/api/auth/login').send({
       username: 'newuser',
       password: '123',
     });
-
-    const token = verifyToken(resLogin.body.token);
 
     const res = await request(app)
       .delete('/api/auth/delete-account')
       .set('Authorization', `Bearer ${resLogin.body.token}`)
       .send({ password: '123' });
 
-    expect(resLogin.status).to.equal(200);
-    expect(resLogin.body.message).to.equal('Login successful!');
-    expect(token.username).to.equal('newuser');
-    expect(res.status).to.equal(200);
-    expect(res.body.message).to.equal('Account deleted successfully!');
+    expect(res.status).to.equal(404);
+    expect(res.body.message).to.equal('User not found.');
+
+    stub.restore();
   });
+
+  // it('should return 500 if DB error occurs /api/auth/delete-account', async () => {
+  //   const stub = sinon.stub(User, 'findById').rejects(new Error('DB failure'));
+
+  //   const resLogin = await request(app).post('/api/auth/login').send({
+  //     username: 'newuser',
+  //     password: '123',
+  //   });
+
+  //   const res = await request(app)
+  //     .delete('/api/auth/delete-account')
+  //     .set('Authorization', `Bearer ${resLogin.body.token}`)
+  //     .send({ password: '123' });
+
+  //   expect(res.status).to.equal(500);
+  //   expect(res.body.error).to.equal('Server error during account deletion.');
+
+  //   stub.restore();
+  // });
+
+  // it('should delete the account /api/auth/delete-account', async () => {
+  //   const resLogin = await request(app).post('/api/auth/login').send({
+  //     username: 'newuser',
+  //     password: '123',
+  //   });
+
+  //   const token = verifyToken(resLogin.body.token);
+
+  //   const res = await request(app)
+  //     .delete('/api/auth/delete-account')
+  //     .set('Authorization', `Bearer ${resLogin.body.token}`)
+  //     .send({ password: '123' });
+
+  //   expect(resLogin.status).to.equal(200);
+  //   expect(resLogin.body.message).to.equal('Login successful!');
+  //   expect(token.username).to.equal('newuser');
+  //   expect(res.status).to.equal(200);
+  //   expect(res.body.message).to.equal('Account deleted successfully!');
+  // });
 });
