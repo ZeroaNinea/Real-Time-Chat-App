5559e0e2cb329542380c37f663b58e9f37a8a3bc
Catgirl <zeroaninea@gmail.com>
2025-08-09 12:31:53 +0400

    don't check the token in the account

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -114,10 +114,6 @@ export const logout = async (req: Request, res: Response) => {
 export const account = async (req: Request, res: Response) => {
   const user = req.user;
 
-  if (!user) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
   res.status(200).json(buildAccountResponse(user));
 };
 
