d6d8034830859e5af6141b3cddbb82de6ab922f0
Catgirl <zeroaninea@gmail.com>
2025-08-09 16:26:28 +0400

    new testing account created

--- backend/test/chat.controller.spec.ts
+++ backend/test/chat.controller.spec.ts
@@ -6,10 +6,33 @@ import fs from 'fs';
 import { app } from '../src/app';
 import { connectToDatabase, disconnectDatabase } from '../src/config/db';
 import { User } from '../src/models/user.model';
+import { verifyToken } from '../src/auth/jwt.service';
 
 describe('Auth Controller', () => {
+  let token;
+
   before(async () => {
     await connectToDatabase();
+
+    const resRegister = await request(app).post('/api/auth/register').send({
+      username: 'newuser',
+      email: 'newuser@email.com',
+      password: '123',
+    });
+
+    expect(resRegister.status).to.equal(201);
+    expect(resRegister.body.message).to.equal('User registered successfully!');
+
+    const resLogin = await request(app).post('/api/auth/login').send({
+      username: 'newuser',
+      password: '123',
+    });
+
+    token = verifyToken(resLogin.body.token);
+
+    expect(resLogin.status).to.equal(200);
+    expect(resLogin.body.message).to.equal('Login successful!');
+    expect(token.username).to.equal('newuser');
   });
 
   after(async () => {
