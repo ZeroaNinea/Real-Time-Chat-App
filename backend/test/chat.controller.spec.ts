d96a30ff0650a16b43cdb32b604b50cbd229465c
Catgirl <zeroaninea@gmail.com>
2025-08-09 17:24:51 +0400

    'should create a new public chat' passed

--- backend/test/chat.controller.spec.ts
+++ backend/test/chat.controller.spec.ts
@@ -6,10 +6,11 @@ import fs from 'fs';
 import { app } from '../src/app';
 import { connectToDatabase, disconnectDatabase } from '../src/config/db';
 import { User } from '../src/models/user.model';
+import { Chat } from '../src/models/chat.model';
 import { verifyToken } from '../src/auth/jwt.service';
 
 describe('Auth Controller', () => {
-  let token;
+  let token: string;
 
   before(async () => {
     await connectToDatabase();
@@ -28,15 +29,28 @@ describe('Auth Controller', () => {
       password: '123',
     });
 
-    token = verifyToken(resLogin.body.token);
+    token = resLogin.body.token;
+
+    const verifiedToken = verifyToken(resLogin.body.token);
 
     expect(resLogin.status).to.equal(200);
     expect(resLogin.body.message).to.equal('Login successful!');
-    expect(token.username).to.equal('newuser');
+    expect(verifiedToken.username).to.equal('newuser');
   });
 
   after(async () => {
     await User.deleteMany({});
+    await Chat.deleteMany({});
     await disconnectDatabase();
+  });
+
+  it('should create a new public chat /api/chat/create-chat', async () => {
+    const res = await request(app)
+      .post('/api/chat/create-chat')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'newchat' });
+
+    expect(res.status).to.equal(201);
+    expect(res.body.name).to.equal('newchat');
   });
 });
