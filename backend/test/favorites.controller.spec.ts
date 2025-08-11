e2837596d5c06eeedb86674420463a60dec566ac
Catgirl <zeroaninea@gmail.com>
2025-08-11 22:12:43 +0400

    favorites.controller.spce.ts created

---
+++ backend/test/favorites.controller.spec.ts
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import sinon from 'sinon';
+import fs from 'fs';
+
+import { app } from '../src/app';
+import mongoose, {
+  connectToDatabase,
+  disconnectDatabase,
+} from '../src/config/db';
+import { User } from '../src/models/user.model';
+// import { Chat } from '../src/models/chat.model';
+// import { Channel } from '../src/models/channel.model';
+// import { Message } from '../src/models/message.model';
+// import { Member } from '../types/member.alias';
+import { verifyToken } from '../src/auth/jwt.service';
+
+describe('Auth Controller', () => {
+  let token: string;
+
+  before(async () => {
+    await connectToDatabase();
+
+    const resRegister = await request(app).post('/api/auth/register').send({
+      username: 'newuser',
+      email: 'newuser@email.com',
+      password: '123',
+    });
+
+    expect(resRegister.status).to.equal(201);
+    expect(resRegister.body.message).to.equal('User registered successfully!');
+
+    const resLogin = await request(app).post('/api/auth/login').send({
+      username: 'newuser',
+      password: '123',
+    });
+
+    token = resLogin.body.token;
+
+    const verifiedToken = verifyToken(resLogin.body.token);
+
+    expect(resLogin.status).to.equal(200);
+    expect(resLogin.body.message).to.equal('Login successful!');
+    expect(verifiedToken.username).to.equal('newuser');
+  });
+
+  after(async () => {
+    await User.deleteMany({});
+    // await Chat.deleteMany({});
+    // await Channel.deleteMany({});
+    // await Message.deleteMany({});
+    await disconnectDatabase();
+  });
+});
