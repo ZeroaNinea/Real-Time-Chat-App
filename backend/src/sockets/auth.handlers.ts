bd9a84198243fe98dfdbb4a5e1eeae1fdaf25b43
Catgirl <zeroaninea@gmail.com>
2025-08-13 23:46:15 +0400

    User import removed

--- backend/src/sockets/auth.handlers.ts
+++ backend/src/sockets/auth.handlers.ts
@@ -1,5 +1,4 @@
 import { Server, Socket } from 'socket.io';
-import { User } from '../models/user.model';
 import userHelper from '../helpers/user-helper';
 
 export function registerAuthHandlers(io: Server, socket: Socket) {
