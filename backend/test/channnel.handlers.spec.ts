fc51fbaaa900528daadb38956060e4804411d153
Catgirl <zeroaninea@gmail.com>
2025-08-13 19:53:30 +0400

    channel.handlers.spec.ts created

---
+++ backend/test/channnel.handlers.spec.ts
@@ -0,0 +1,64 @@
+import { expect } from 'chai';
+import { createServer } from 'http';
+import sinon from 'sinon';
+import request from 'supertest';
+
+import { app } from '../src/app';
+
+import { io as Client, Socket as ClientSocket } from 'socket.io-client';
+import { Server } from 'socket.io';
+
+import { connectToDatabase, disconnectDatabase } from '../src/config/db';
+import { setupSocket } from '../src/socket';
+import { User } from '../src/models/user.model';
+import userHelper from '../src/helpers/user-helper';
+
+describe('Auth Socket Handlers', () => {
+  let server: ReturnType<typeof createServer>;
+  let address: string;
+  let clientSocket: ClientSocket;
+  let io: Server;
+  let user: typeof User;
+  let token: string;
+
+  before(async () => {
+    await connectToDatabase();
+
+    user = await User.create({
+      username: 'socketuser',
+      email: 'socket@email.com',
+      password: '123',
+      status: 'offline',
+    });
+
+    const resLogin = await request(app).post('/api/auth/login').send({
+      username: 'socketuser',
+      password: '123',
+    });
+
+    token = resLogin.body.token;
+
+    const resCreateChat = await request(app)
+      .post('/api/chat/create-chat')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'newchat' });
+
+    server = createServer(app);
+    io = setupSocket(server, app);
+
+    await new Promise<void>((resolve) => {
+      server.listen(() => {
+        const port = (server.address() as any).port;
+        address = `http://localhost:${port}`;
+        resolve();
+      });
+    });
+  });
+
+  after(async () => {
+    await User.deleteMany({});
+    await disconnectDatabase();
+    io.close();
+    server.close();
+  });
+});
