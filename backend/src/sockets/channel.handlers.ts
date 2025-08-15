3645c6054d8df76fbd1f60a469f67016ad84dcfc
Catgirl <zeroaninea@gmail.com>
2025-08-15 11:51:01 +0400

    returns removed

--- backend/src/sockets/channel.handlers.ts
+++ backend/src/sockets/channel.handlers.ts
@@ -226,15 +226,16 @@ export function registerChannelHandlers(io: Server, socket: Socket) {
     try {
       const chat = await Chat.findById(chatId);
       if (!chat) {
-        return callback?.({ error: 'Chat is not found.' });
+        callback?.({ error: 'Chat is not found.' });
       }
 
       const member = chat.members.find((m: Member) =>
         m.user.equals(socket.data.user._id)
       );
 
-      if (!member)
-        return callback?.({ error: 'You are not a member of this chat.' });
+      if (!member) {
+        callback?.({ error: 'You are not a member of this chat.' });
+      }
 
       const memberRoles = member.roles.map((role: string) => {
         return chat.roles.find((r: Role) => r.name === role);
@@ -256,7 +257,7 @@ export function registerChannelHandlers(io: Server, socket: Socket) {
       );
 
       if (!hasChannelEditPermissions) {
-        return callback?.({
+        callback?.({
           error: 'You are not allowed to change the channel order.',
         });
       }
