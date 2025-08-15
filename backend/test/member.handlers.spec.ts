dff781a7e6401ceb3c47a8804b2903b1add7d3e9
Catgirl <zeroaninea@gmail.com>
2025-08-16 02:50:03 +0400

    'should fail to delete the Owner role' added

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -1819,6 +1819,38 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should fail to delete the Owner role', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: chat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(chat._id.toString());
+
+        clientSocket.emit(
+          'deleteRole',
+          {
+            chatId: chat._id,
+            role: 'Owner',
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal(
+              'You cannot delete roles higher or equal to your own.'
+            );
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   it('should delete the Removing-Role', (done) => {
     clientSocket = Client(address, {
       auth: { token: token },
