18c9f9beb78fa5fef6ae40b9c8880c44d7133839
Catgirl <zeroaninea@gmail.com>
2025-08-16 02:25:52 +0400

    'should delete the Removing-Role' added

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -1758,4 +1758,47 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should delete the Removing-Role', (done) => {
+    clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: chat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(chat._id.toString());
+
+        clientSocket.emit(
+          'deleteRole',
+          {
+            chatId: chat._id,
+            role: 'Removing-Role',
+          },
+          (response: any) => {
+            console.log(
+              response,
+              '============================================'
+            );
+            // expect(response.success).to.equal(true);
+            // expect(response.member.roles.includes('Removing-Role')).to.equal(
+            //   false
+            // );
+            clientSocket.disconnect();
+            done();
+          }
+        );
+
+        // clientSocket.on('memberUpdated', (response) => {
+        //   expect(response.roles.includes('Removing-Role')).to.equal(false);
+        //   clientSocket.disconnect();
+        //   done();
+        // });
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
