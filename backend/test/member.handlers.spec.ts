5bc52b5e1fdd0536a3f8464f9d941295c0663991
Catgirl <zeroaninea@gmail.com>
2025-08-15 20:04:40 +0400

    'should return a server error during role editing' passed

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -566,5 +566,42 @@ describe('Auth Socket Handlers', () => {
         });
       });
     });
+
+    clientSocket.on('connect_error', done);
+  });
+
+  it('should return a server error during role editing', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: chat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        const stub = sinon.stub(Chat, 'findById').throws(new Error('DB down'));
+        expect(chatId).to.equal(chat._id.toString());
+
+        clientSocket.emit(
+          'editRole',
+          {
+            chatId: chat._id,
+            role: {
+              name: 'newrole',
+              description: 'newrole',
+            },
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal('Server error during role update.');
+            clientSocket.disconnect();
+            stub.restore();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
   });
 });
