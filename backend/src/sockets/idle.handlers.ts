9d3c2bd7be596c053366d0f1a03148a40bf3f086
Catgirl <zeroaninea@gmail.com>
2025-08-15 13:25:57 +0400

    don't check user ID

--- backend/src/sockets/idle.handlers.ts
+++ backend/src/sockets/idle.handlers.ts
@@ -4,14 +4,12 @@ export function registerIdleHandlers(io: Server, socket: Socket) {
   socket.on('userIdle', () => {
     const userId = socket.data.user._id.toString();
 
-    if (!userId) return;
     io.emit('userIdle', userId);
   });
 
   socket.on('userActive', () => {
     const userId = socket.data.user._id.toString();
 
-    if (!userId) return;
     io.emit('userActive', userId);
   });
 }
