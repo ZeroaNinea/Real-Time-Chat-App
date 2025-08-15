dee5633c93c4982e669cf328a72e18bd5dc13b61
Catgirl <zeroaninea@gmail.com>
2025-08-15 23:38:51 +0400

    'should not allow to toggle a non-existing role' added

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -1240,4 +1240,35 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should not allow to toggle a non-existing role', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: chat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(chat._id.toString());
+
+        clientSocket.emit(
+          'toggleRole',
+          {
+            chatId: chat._id,
+            roleName: 'Fake-Role',
+            selected: true,
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal('Role is not found.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
