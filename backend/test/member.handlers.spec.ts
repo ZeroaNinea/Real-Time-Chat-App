0f328644d4067073b20e263291028b56c8ca4150
Catgirl <zeroaninea@gmail.com>
2025-08-16 15:21:11 +0400

    'should not allow user4 to become a member of the chat room again' added

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -2269,4 +2269,35 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should not allow user4 to become a member of the chat room again', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token4 },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: chat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(chat._id.toString());
+
+        clientSocket.emit(
+          'becomeMember',
+          {
+            chatId: chat._id,
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal(
+              'You are already a member of this chat.'
+            );
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
