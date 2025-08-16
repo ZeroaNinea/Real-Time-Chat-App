9ae75c6c8b85afe9ab0e2a48e8bb0594ee697d42
Catgirl <zeroaninea@gmail.com>
2025-08-16 13:53:33 +0400

    'should fail to leave the private chat room' added

--- backend/test/member.handlers.spec.ts
+++ backend/test/member.handlers.spec.ts
@@ -2020,6 +2020,35 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should fail to leave the private chat room', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(user._id.toString());
+
+        clientSocket.emit(
+          'leaveChatRoom',
+          {
+            chatId: privateChat._id,
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal('You cannot leave a private chat.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   it('should leave the chat room', (done) => {
     clientSocket = Client(address, {
       auth: { token: token4 },
