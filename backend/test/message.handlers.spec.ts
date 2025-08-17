684d47ae8734863fc33fb2cd01a19118e6c26e38
Catgirl <zeroaninea@gmail.com>
2025-08-17 21:44:49 +0400

    'should reply the messasge' added

--- backend/test/message.handlers.spec.ts
+++ backend/test/message.handlers.spec.ts
@@ -860,6 +860,40 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should reply the messasge', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: privateChat._id });
+
+      clientSocket.on('roomJoined', async ({ chatId }) => {
+        const message = await Message.findOne({
+          text: 'new message',
+          chatId: chat._id,
+        });
+        expect(chatId).to.equal(privateChat._id.toString());
+
+        clientSocket.emit(
+          'reply',
+          {
+            messageId: message._id,
+            text: 'new message',
+          },
+          (err: { error: string }) => {
+            expect(err).to.equal(undefined);
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   // Delete Message
 
   it('should not allow user2 to delete the message', (done) => {
