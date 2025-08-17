1c19bb90d66410d57fd096a11a5ff3946087c878
Catgirl <zeroaninea@gmail.com>
2025-08-17 15:41:21 +0400

    'should return message is not found if the message ID is incorrect' added

--- backend/test/message.handlers.spec.ts
+++ backend/test/message.handlers.spec.ts
@@ -691,4 +691,33 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should return message is not found if the message ID is incorrect', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: privateChat._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(privateChat._id.toString());
+
+        clientSocket.emit(
+          'deleteMessage',
+          {
+            messageId: new mongoose.Types.ObjectId(),
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal('Message is not found.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
