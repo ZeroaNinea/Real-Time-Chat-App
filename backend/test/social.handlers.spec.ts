87a1bd9fef59aaf89000f685eea27829b57abd92
Catgirl <zeroaninea@gmail.com>
2025-08-20 19:08:46 +0400

    'should return user is not found if there is no user with the given ID during removing friend' added

--- backend/test/social.handlers.spec.ts
+++ backend/test/social.handlers.spec.ts
@@ -594,6 +594,33 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should return user is not found if there is no user with the given ID during removing friend', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user2._id });
+
+      clientSocket.on('roomJoined', async ({ chatId }) => {
+        expect(chatId).to.equal(user2._id.toString());
+
+        clientSocket.emit(
+          'removeFriend',
+          new mongoose.Types.ObjectId().toString(),
+          (err: { error: string }) => {
+            expect(err.error).to.equal('User is not found.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   it('should remove friend', (done) => {
     const clientSocket = Client(address, {
       auth: { token: token },
