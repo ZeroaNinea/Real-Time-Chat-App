3f513cadb80983fdc7338208b1ec304c53d574a2
Catgirl <zeroaninea@gmail.com>
2025-08-21 17:58:33 +0400

    'should return a server error during declining a private chat deletion request' added

--- backend/test/social.handlers.spec.ts
+++ backend/test/social.handlers.spec.ts
@@ -1616,6 +1616,42 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should return a server error during declining a private chat deletion request', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token2 },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        const stub = sinon
+          .stub(Notification, 'findOneAndDelete')
+          .rejects(new Error('DB down'));
+        expect(chatId).to.equal(user._id.toString());
+
+        clientSocket.emit(
+          'declinePrivateChatDeletion',
+          {
+            recipientId: user._id,
+            chatId: privateChat._id,
+          },
+          (err: { error: string }) => {
+            expect(err.error).to.equal(
+              'Server error during declining deletion request.'
+            );
+            clientSocket.disconnect();
+            stub.restore();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   it('should decline a private chat deletion request', (done) => {
     const clientSocket = Client(address, {
       auth: { token: token2 },
