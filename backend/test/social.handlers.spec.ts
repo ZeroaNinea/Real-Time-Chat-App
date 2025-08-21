5351e49d7f0a7ecf98ea80a8fa0a756c4d364769
Catgirl <zeroaninea@gmail.com>
2025-08-21 12:52:02 +0400

    'should return user is not found during unbanning a user' added

--- backend/test/social.handlers.spec.ts
+++ backend/test/social.handlers.spec.ts
@@ -1225,4 +1225,31 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should return user is not found during unbanning a user', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user._id });
+
+      clientSocket.on('roomJoined', async ({ chatId }) => {
+        expect(chatId).to.equal(user._id.toString());
+
+        clientSocket.emit(
+          'unbanUser',
+          new mongoose.Types.ObjectId(),
+          (err: { error: string }) => {
+            expect(err.error).to.equal('User is not found.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
