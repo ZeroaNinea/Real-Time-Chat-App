e66a99f7739a52da8ef79757016aeef64b60fcac
Catgirl <zeroaninea@gmail.com>
2025-08-21 12:17:17 +0400

    'should return invalid user ID during banning a user' added

--- backend/test/social.handlers.spec.ts
+++ backend/test/social.handlers.spec.ts
@@ -997,6 +997,33 @@ describe('Auth Socket Handlers', () => {
     clientSocket.on('connect_error', done);
   });
 
+  it('should return invalid user ID during banning a user', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user._id });
+
+      clientSocket.on('roomJoined', async ({ chatId }) => {
+        expect(chatId).to.equal(user._id.toString());
+
+        clientSocket.emit(
+          'banUser',
+          { userId: new mongoose.Types.ObjectId().toString() },
+          (err: { error: string }) => {
+            expect(err.error).to.equal('Invalid user ID.');
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
+
   it('should ban user2', (done) => {
     const clientSocket = Client(address, {
       auth: { token: token },
