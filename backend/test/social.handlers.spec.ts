bb00ca7eca1303c96d9d1f89ba22d63f24788e2b
Catgirl <zeroaninea@gmail.com>
2025-08-21 18:20:18 +0400

    'should confirm a private chat deletion request' added

--- backend/test/social.handlers.spec.ts
+++ backend/test/social.handlers.spec.ts
@@ -1721,4 +1721,34 @@ describe('Auth Socket Handlers', () => {
 
     clientSocket.on('connect_error', done);
   });
+
+  it('should confirm a private chat deletion request', (done) => {
+    const clientSocket = Client(address, {
+      auth: { token: token2 },
+      transports: ['websocket'],
+    });
+
+    clientSocket.on('connect', () => {
+      clientSocket.emit('joinChatRoom', { chatId: user._id });
+
+      clientSocket.on('roomJoined', ({ chatId }) => {
+        expect(chatId).to.equal(user._id.toString());
+
+        clientSocket.emit(
+          'confirmPrivateChatDeletion',
+          {
+            recipientId: user._id,
+            chatId: privateChat._id,
+          },
+          (response: { success: boolean }) => {
+            expect(response.success).to.equal(true);
+            clientSocket.disconnect();
+            done();
+          }
+        );
+      });
+    });
+
+    clientSocket.on('connect_error', done);
+  });
 });
