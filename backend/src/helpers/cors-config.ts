13326e9fa07ab54fa88cedca5ab492ff0b297535
Catgirl <zeroaninea@gmail.com>
2026-03-23 22:57:17 +0400

    new domain added in cors-config.ts

--- backend/src/helpers/cors-config.ts
+++ backend/src/helpers/cors-config.ts
@@ -2,7 +2,11 @@ export const corsOriginValidator = (
   origin: string | undefined,
   callback: (err: Error | null, allow?: boolean) => void,
 ) => {
-  const allowed = ['http://localhost:4200', 'https://real-time-chat-app.local'];
+  const allowed = [
+    'http://localhost:4200',
+    'https://real-time-chat-app.local',
+    'add_the_future_domain_here',
+  ];
 
   if (!origin || allowed.includes(origin)) {
     callback(null, true);
