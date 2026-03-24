2657f154697be21c31a3a74caf5296174c59663a
Catgirl <zeroaninea@gmail.com>
2026-03-24 20:11:01 +0400

    backend url added

--- frontend/src/environments/environment(rename).ts
+++ frontend/src/environments/environment(rename).ts
@@ -1,6 +1,14 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:5000',
+
+  // Development
+  backendUrl: 'http://localhost:5000/api',
+  socketUrl: 'http://localhost:5000',
+
+  // Production
+  // backendUrl: 'https://real-time-chat-app.local/api',
+  // socketUrl: 'https://real-time-chat-app.local',
+
   tenorApiKey: '<YOUR_TENOR_API_KEY>',
 };
 // Replace <YOUR_TENOR_API_KEY> with your actual Tenor API key.
