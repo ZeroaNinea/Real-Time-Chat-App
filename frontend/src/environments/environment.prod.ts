d326fbe208daad4328dad951295d3a3d6f3266c4
Catgirl <zeroaninea@gmail.com>
2026-03-24 23:20:14 +0400

    tenor no longer a secret

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+
+  backendUrl: 'https://real-time-chat-app-ga5a.onrender.com/api',
+  socketUrl: 'https://real-time-chat-app-ga5a.onrender.com',
+
+  tenorApiKey: 'AIzaSyCeUgqzaZhxMmo_mgpkeXbXgFAfm8zoAX4',
+};
