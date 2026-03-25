10aaefef4e54ac24c36c810a63fb9240dc20cd20
Catgirl <zeroaninea@gmail.com>
2026-03-25 13:01:59 +0400

    trying to deploy on vercel

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+
+  // Development
+  // backendUrl: 'http://localhost:5000/api',
+  // socketUrl: 'http://localhost:5000',
+
+  // Production
+  // backendUrl: 'https://real-time-chat-app.local/api',
+  // socketUrl: 'https://real-time-chat-app.local',
+  backendUrl: 'https://real-time-chat-app-ga5a.onrender.com/api/',
+  socketUrl: 'https://real-time-chat-app-ga5a.onrender.com',
+
+  tenorApiKey: 'AIzaSyCeUgqzaZhxMmo_mgpkeXbXgFAfm8zoAX4',
+};
