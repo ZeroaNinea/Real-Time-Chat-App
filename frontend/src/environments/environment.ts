2f89b074e3709fa05b1e59b1c88450f701d00433
Catgirl <zeroaninea@gmail.com>
2026-04-30 22:10:02 +0400

    run the frontend in development mode

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: false,
 
   // Development
-  // backendUrl: 'http://localhost:5000/api',
-  // socketUrl: 'http://localhost:5000',
+  backendUrl: 'http://localhost:5000/api',
+  socketUrl: 'http://localhost:5000',
 
   // Production
   // backendUrl: 'https://real-time-chat-app.local/api',
   // socketUrl: 'https://real-time-chat-app.local',
-  backendUrl: 'https://real-time-chat-app-ga5a.onrender.com/api/',
-  socketUrl: 'https://real-time-chat-app-ga5a.onrender.com',
+  // backendUrl: 'https://real-time-chat-app-ga5a.onrender.com/api',
+  // socketUrl: 'https://real-time-chat-app-ga5a.onrender.com',
 
   tenorApiKey: 'AIzaSyCeUgqzaZhxMmo_mgpkeXbXgFAfm8zoAX4',
 };
